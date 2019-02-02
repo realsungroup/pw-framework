4d908536a2a59ccb37ba8393e3883f0fd5c9efe5
hileix <hileix@MacdeMacBook-Pro.local>
2019-02-02 15:22:54 +0800

    fix:修复 ant-design 样式未导入的 bug

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import './index.css';
+import './index.less';
+
 
 // 使用自定义 loading
 import loadingGif from './assets/loading.gif';
