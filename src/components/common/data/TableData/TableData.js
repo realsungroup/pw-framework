4513ae2771d93a3d1f5a3a90daf6da8ee070e08f
hileix <304192604@qq.com>
2019-03-16 16:47:42 +0800

    fix:修复只有按钮时，点后端按钮不会弹出记录表单

--- src/components/common/data/TableData/TableData.js
+++ src/components/common/data/TableData/TableData.js
@@ -627,8 +627,12 @@ class TableData extends React.Component {
 
     const { recordFormShowMode, selectedRecord } = this.state;
     const { intl } = this.props;
-    if (!this._recordFormData || !this._dealedRecordFormData) {
-      return message.info('正在请求窗体数据，请稍等...');
+
+    // 点击前端按钮时
+    if (!backendBtnType) {
+      if (!this._recordFormData || !this._dealedRecordFormData) {
+        return message.info('正在请求窗体数据，请稍等...');
+      }
     }
 
     const newOperation = operation || recordFormShowMode;
@@ -873,19 +877,38 @@ class TableData extends React.Component {
     type,
     records,
     controlData,
-    defaultRecord
+    defaultRecord,
+    recordFormData
   ) => {
     if (type === 1 || type === 5) {
       this.handleRefresh();
       // 编辑记录
     } else if (type === 6) {
-      this.openRecordForm(backendBtnType, 'modify', defaultRecord, controlData);
+      this.openRecordForm(
+        backendBtnType,
+        'modify',
+        defaultRecord,
+        controlData,
+        recordFormData
+      );
       // 查看记录
     } else if (type === 7) {
-      this.openRecordForm(backendBtnType, 'view', records[0], controlData);
+      this.openRecordForm(
+        backendBtnType,
+        'view',
+        records[0],
+        controlData,
+        recordFormData
+      );
       // 添加记录
     } else if (type === 8) {
-      this.openRecordForm(backendBtnType, 'add', defaultRecord, controlData);
+      this.openRecordForm(
+        backendBtnType,
+        'add',
+        defaultRecord,
+        controlData,
+        recordFormData
+      );
     }
   };
 
