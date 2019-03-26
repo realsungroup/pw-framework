cdc547c3cf5b804cbc129c39406cf10dd56d9b09
hileix <304192604@qq.com>
2019-03-26 17:43:34 +0800

    feat:添加 bi 系统基地址配置

--- src/pages/Home/HomeBody/HomeDashboard/HomeDashboard.js
+++ src/pages/Home/HomeBody/HomeDashboard/HomeDashboard.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import './HomeDashboard.less';
-
+import { Button } from 'antd';
 
 export default class HomeDashboard extends React.PureComponent {
   render() {
     const { defaultDashboard } = this.props;
-    if (!defaultDashboard) {
-      return <div className="home-dashboard">设置默认仪表盘</div>;
+    if (defaultDashboard) {
+      return (
+        <div className="home-dashboard">
+          <p className="home-dashboard__go-to-setting">
+            <p className="home-dashboard__no-default-dashboard">
+              暂无默认仪表盘
+            </p>
+            <Button type="primary">去设置默认仪表盘</Button>
+          </p>
+        </div>
+      );
     }
 
-    return <div className="home-dashboard">home-dashboard</div>;
+    return (
+      <div className="home-dashboard">{/* <DashboardPage rows={} /> */}</div>
+    );
   }
 }
