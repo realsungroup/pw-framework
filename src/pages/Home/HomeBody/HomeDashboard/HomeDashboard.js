feedd41009897fdbec11cc2078c68385a66fbbb4
hileix <304192604@qq.com>
2019-03-26 18:33:17 +0800

    feat:完成跳转到 bi 系统的功能

--- src/pages/Home/HomeBody/HomeDashboard/HomeDashboard.js
+++ src/pages/Home/HomeBody/HomeDashboard/HomeDashboard.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import './HomeDashboard.less';
 import { Button } from 'antd';
+import { getItem } from 'Util20/util';
+
+const biBaseURL = window.pwConfig[process.env.NODE_ENV].biBaseURL;
+
+const getHref = biBaseURL => {
+  const userInfo = JSON.parse(getItem('userInfo'));
+  const { AccessToken, UserCode } = userInfo;
+  return `${biBaseURL}/home?u=${UserCode}&a=${AccessToken}`;
+};
 
 export default class HomeDashboard extends React.PureComponent {
   render() {
     const { defaultDashboard } = this.props;
+
     if (defaultDashboard) {
+      const url = getHref(biBaseURL);
+
       return (
         <div className="home-dashboard">
           <p className="home-dashboard__go-to-setting">
             <p className="home-dashboard__no-default-dashboard">
               暂无默认仪表盘
             </p>
-            <Button type="primary">去设置默认仪表盘</Button>
+            <Button href={biBaseURL} target="black" type="primary">
+              去设置默认仪表盘
+            </Button>
           </p>
         </div>
       );
