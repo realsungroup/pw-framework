c3a222a213c228dbacb546c153bb0d68b7065be8
hileix <304192604@qq.com>
2019-03-28 17:22:46 +0800

    feat:引入 lz-components-and-utils package

--- src/pages/Home/HomeBody/HomeDashboard/HomeDashboard.js
+++ src/pages/Home/HomeBody/HomeDashboard/HomeDashboard.js
@@ -2,8 +2,10 @@ import React from 'react';
 import './HomeDashboard.less';
 import { Button } from 'antd';
 import { getItem } from 'Util20/util';
+import DashboardPage from 'lz-components-and-utils/lib/DashboardPage';
 
 const biBaseURL = window.pwConfig[process.env.NODE_ENV].biBaseURL;
+const baseURL = window.pwConfig[process.env.NODE_ENV].baseURL;
 
 const getHref = biBaseURL => {
   const userInfo = JSON.parse(getItem('userInfo'));
@@ -15,7 +17,7 @@ export default class HomeDashboard extends React.PureComponent {
   render() {
     const { defaultDashboard } = this.props;
 
-    if (defaultDashboard) {
+    if (!defaultDashboard) {
       const url = getHref(biBaseURL);
 
       return (
@@ -24,7 +26,7 @@ export default class HomeDashboard extends React.PureComponent {
             <p className="home-dashboard__no-default-dashboard">
               暂无默认仪表盘
             </p>
-            <Button href={biBaseURL} target="black" type="primary">
+            <Button href={url} target="black" type="primary">
               去设置默认仪表盘
             </Button>
           </p>
@@ -32,8 +34,21 @@ export default class HomeDashboard extends React.PureComponent {
       );
     }
 
+    let rows;
+    try {
+      rows = JSON.parse(defaultDashboard.ReportConfig);
+    } catch (err) {
+      return <div>dashboard 配置信息有误</div>;
+    }
+
+    if (!Array.isArray(rows)) {
+      return <div>dashboard 配置信息有误</div>;
+    }
+
     return (
-      <div className="home-dashboard">{/* <DashboardPage rows={} /> */}</div>
+      <div className="home-dashboard">
+        <DashboardPage baseURL={baseURL} rows={rows} mode="view" />
+      </div>
     );
   }
 }
