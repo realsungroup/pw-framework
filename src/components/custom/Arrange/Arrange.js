df521b3b7ebfad272cd2742fc6a16a1ea4d5ed73
13627491210 <782572516@qq.com>
2019-03-30 15:25:18 +0800

    '加了一个tabs'

--- src/components/custom/Arrange/Arrange.js
+++ src/components/custom/Arrange/Arrange.js
@@ -1,104 +1,56 @@
 import React, { Component } from "react";
 import "./Arrange.less";
-import { Modal, Button, Input ,DatePicker,Radio } from "antd";
+import {Tabs,Modal, Button} from "antd";
 
-const Search = Input.Search;
-const { MonthPicker, RangePicker, WeekPicker } = DatePicker;
-const RadioGroup = Radio.Group;
-
-function onChange(date, dateString) {
-  console.log(date, dateString);
+const TabPane = Tabs.TabPane;
+function callback(key) {
+  console.log(key);
 }
 
 export default class Arrange extends Component {
-  state = { visible: false,value:1 ,wid:700};
+  state = { visible: false }
 
   showModal = () => {
     this.setState({
-      visible: true
-    });
-  };
-
-  onChange = (e) => {
-    console.log('radio checked', e.target.value);
-    this.setState({
-      value: e.target.value,
+      visible: true,
     });
   }
 
-  handleOk = e => {
+  handleOk = (e) => {
     console.log(e);
     this.setState({
-      visible: false
+      visible: false,
     });
-  };
-
-  handleCancel = e => {
-    console.log(e);
-    this.setState({
-      visible: false
-    });
-  };
-  render() {
-    return (
-      <div>
-        <Button type="primary" onClick={this.showModal}>
-          添加考试安排
-        </Button>
-        <Modal 
-          //  className='contain'
-           width={this.state.wid}
-          title="考试安排"
-          visible={this.state.visible}
-          onOk={this.handleOk}
-          onCancel={this.handleCancel}
-        >
-          <div className="modal">
-            <label>安排分类:</label>
-            <Search
-              onSearch={value => console.log(value)}
-              style={{ width: 200 }}
-            />
-          </div>
-          <div className='list modal'>
-            <label>考试名称:</label>
-            <Input className="input" />
-          </div>
-          <div className='list modal'>
-            <label>考试时间:</label>
-            <Input className='input'></Input>
-          </div>
-          <div className='list modal'>
-            <label>参加次数:</label>
-            <Input className='input'></Input>
-          </div>
-          <div className='list modal'>
-            <label>试卷分数:</label>
-            <Input className='input'></Input>
-          </div>
-          <div className='list modal'>
-            <label>通过分数:</label>
-            <Input className='input'></Input>
-          </div>
-          <div className='list modal'>
-            <label>有效时间:</label>
-            <RangePicker onChange={onChange} />
-          </div>
-          <div className='list modal'>
-            <label>成绩公布安排:</label>
-            <Radio>即时显示考卷分数</Radio>
-            <Radio>指定成绩发布时间</Radio>
-            <DatePicker onChange={onChange} />
-          </div>
-          <div className='list modal'>
-            <label>选择试卷</label>
-            <Search
-              onSearch={value => console.log(value)}
-              style={{ width: 200 }}
-            />
-          </div>
-        </Modal>
-      </div>
-    );
   }
+
+  handleCancel = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  }
+
+render(){
+  return (
+
+  <div>
+  <Button type="primary" onClick={this.showModal}>添加题型
+  </Button>
+  <Modal
+    title="添加题目"
+    visible={this.state.visible}
+    onOk={this.handleOk}
+    onCancel={this.handleCancel}
+  >
+<Tabs defaultActiveKey="1" onChange={callback}>
+    <TabPane tab="单选题" key="1"></TabPane>
+    <TabPane tab="多选题" key="2"></TabPane>
+    <TabPane tab="判断题" key="3"></TabPane>
+    <TabPane tab="填空题" key="4"></TabPane>
+    <TabPane tab="问答题" key="5"></TabPane>
+  </Tabs>
+  </Modal>
+</div>
+  )
+}
 }
