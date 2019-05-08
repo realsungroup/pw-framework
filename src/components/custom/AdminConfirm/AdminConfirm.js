34303feda218434d5e5ab2f7970b6f45db5ca35a
hileix <304192604@qq.com>
2019-05-08 11:32:53 +0800

    feat:管理员确认页面添加前端按钮

---
+++ src/components/custom/AdminConfirm/AdminConfirm.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { TableData } from '../../common/loadableCommon';
+import { Button, Popconfirm, message, Spin } from 'antd';
+import http from 'Util20/api';
+
+/**
+ * 管理员确认
+ */
+class AdminConfirm extends React.Component {
+  state = {
+    loading: false
+  };
+
+  handleConfirm = async (dataSource, selectedRowKeys) => {
+    const { resid } = this.props;
+    this.setState({ loading: true });
+    const data = selectedRowKeys.map(recid => ({
+      REC_ID: recid,
+      C3_605619907534: 'Y'
+    }));
+
+    let res;
+    try {
+      res = await http().modifyRecords({
+        resid,
+        data
+      });
+    } catch (err) {
+      this.setState({ loading: false });
+      console.error(err);
+      return message.error(err.message);
+    }
+    this.setState({ loading: false });
+    message.success('操作成功');
+    this.tableDataRef.handleRefresh();
+  };
+
+  renderActionBarExtra = ({ dataSource, selectedRowKeys }) => {
+    return (
+      <Popconfirm
+        title="您确定要操作吗？"
+        onConfirm={() => this.handleConfirm(dataSource, selectedRowKeys)}
+      >
+        <Button>确认</Button>
+      </Popconfirm>
+    );
+  };
+
+  render() {
+    const { loading } = this.state;
+    return (
+      <Spin spinning={loading}>
+        <div
+          className="table-data-wrap"
+          style={{ height: 'calc(100vh - 160px)' }}
+        >
+          <TableData
+            {...this.props}
+            actionBarExtra={this.renderActionBarExtra}
+            wrappedComponentRef={element => (this.tableDataRef = element)}
+            refTargetComponentName="TableData"
+          />
+        </div>
+      </Spin>
+    );
+  }
+}
+
+export default AdminConfirm;
