af8191f09bf606aee957cc532470e7f995ccaf63
dengming <13767267543@163.com>
2019-06-12 16:34:24 +0800

    fix 进度显示有问题

--- src/components/custom/CreateTotalPlan/CreateTotalPlan.js
+++ src/components/custom/CreateTotalPlan/CreateTotalPlan.js
@@ -82,6 +82,7 @@ class CreateTotalPlan extends React.Component {
       this.setState({
         totalIndex: res.data.Total,
         curIndex: res.data.Index,
+        isTaskComplete: false
       });
       this.timer = setTimeout(async () => {
         if (this.getTaskInfo) {
