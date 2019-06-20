e0c8843c8eeac05c5de3ab0aca4d15e6a5044802
povernly <clg11504@sina.com>
2019-06-20 18:14:50 +0800

    申请订单通知，新增搜索全部通知

--- src/components/custom/ExternalTraining/ETnoticeMain/ETNoticed/ETNoticed.js
+++ src/components/custom/ExternalTraining/ETnoticeMain/ETNoticed/ETNoticed.js
@@ -1,37 +1,30 @@
 import React from 'react';
-import { Skeleton, Switch, Card, Icon, Avatar, Radio, Spin, Button, Progress, Tabs, Modal, Popconfirm, message } from 'antd';
+import { Card, Icon, Spin, Button, Progress, Modal, Popconfirm, message, Checkbox } from 'antd';
 import './ETNoticed.less';
 import http from 'Util20/api';
 
 
 class ETNoticed extends React.Component {
     count = 0
-
+    state = {
+        modalVisible: false,
+        currentCourse: {}, //当前点击的课程  
+        currentCourseIndex:Number,//当前点击的课程在courseNotApply的index
+        currentCourseAllPeople: [], //当前选中课程的所有人
+        peopleIsApply: [],  //当前选中课程已通知的人
+        peopleNotApply: [], //当前选中F课程没通知的人
+        peopleForNotApply: [], //当前选中课程通知失败的人
+        percent: 0,
+    }
     constructor(props) {
         super(props)
     }
-    state = {
-        isNotice: 0,
-        modalVisible: false,
-        courseArrangment: [],
-        courseIsApply: [], //已经通知过的课程
-        courseNotApply: [], //未通知过的课程
-        currentCourse: {}, //当前点击的课程
-        currentCourseAllPeople: [], //当前选中课程的所有人
-        peopleIsApply: [],  //当前选中课程已通知的人
-        peopleNotApply: [], //当前选中课程没通知的人
-        peopleForNotApply: [], //当前选中课程通知失败的人
-        percent: 0,
-        loading:false
-    }
     componentWillMount = () => {
-        this.setState({
-            isNotice: this.props.isNotice
-        })
     }
     componentDidMount = () => {
-        this.setState({ loading: true });
-        this.getCourseArrangment()
+        this.setState({
+            isNotice: this.props.isNotice,
+        })
     }
     handleOk = e => {
         this.setState({
@@ -41,7 +34,8 @@ class ETNoticed extends React.Component {
             peopleNotApply: [],
             peopleIsApply: [],
             percent: 0,
-            currentCourse: {}
+            currentCourse: {},
+            currentCourseIndex:Number
         });
     };
 
@@ -53,36 +47,38 @@ class ETNoticed extends React.Component {
             peopleNotApply: [],
             peopleForNotApply: [],
             percent: 0,
-            currentCourse: {}
+            currentCourse: {},
+            currentCourseIndex:Number
         });
     };
 
-    getCourseArrangment = async () => {
-        let res;
-        try {
-            res = await http().getTable({
-                resid: '613959525708'
-            });
-        } catch (error) {
-            return console.log(error);
-        }
-        if (res.error === 0) {
-            let courseArrangment = res.data;
-            let courseIsApply = []
-            let courseNotApply = []
-            courseArrangment.forEach((item) => {
-                if (item.C3_614256491795 == 'Y') {
-                    courseIsApply.push(item)
-                } else {
-                    courseNotApply.push(item)
-                }
-            })
-            this.setState({ courseArrangment, courseIsApply, courseNotApply ,loading:false});
-        }
-    };
-    getCurrentCourseIsApply = async (e) => {
+    // getCourseArrangment = async () => {
+    //     let res;
+    //     try {
+    //         res = await http().getTable({
+    //             resid: '613959525708'
+    //         });
+    //     } catch (error) {
+    //         return console.log(error);
+    //     }
+    //     if (res.error === 0) {
+    //         let courseArrangment = res.data;
+    //         let courseIsApply = []
+    //         let courseNotApply = []
+    //         courseArrangment.forEach((item) => {
+    //             if (item.C3_614256491795 == 'Y') {
+    //                 courseIsApply.push(item)
+    //             } else {
+    //                 courseNotApply.push(item)
+    //             }
+    //         })
+    //         this.setState({ courseArrangment, courseIsApply, courseNotApply ,loading:false});
+    //     }
+    // };
+    getCurrentCourseIsApply = async (e,i) => {
         this.setState({
-            currentCourse: e
+            currentCourse: e,
+            currentCourseIndex:i
         })
         let res;
         try {
@@ -95,6 +91,15 @@ class ETNoticed extends React.Component {
         }
         if (res.error === 0) {
             let currentCourseAllPeople = res.data;
+            if (!currentCourseAllPeople.length) {
+                message.error('所选课程没有需要通知的人')
+                this.setState({
+                    currentCourse: {},
+                    currentCourseIndex:Number
+                })
+                return
+            }
+            this.setState({ modalVisible: true })
             let peopleIsApply = []
             let peopleNotApply = []
             currentCourseAllPeople.forEach(element => {
@@ -168,115 +173,129 @@ class ETNoticed extends React.Component {
                 } catch (error) {
                     console.error(error)
                 }
-                if (resm.Error != 0) {
+                
+                if (resm.Error === 0) {
+                    this.handleCurrentCourseIsApply(this.state.currentCourse,this.state.currentCourseIndex)
+                }else{
                     message.error(resm.message)
+
                 }
             }
         } else {
             this.modifyCurrentCourseIsApply()
         }
     }
+    handleCurrentCourseIsApply(e,i){
+        this.props.currentCourseIsApply(e,i)
+    }
     render() {
         return (
-            <Spin  spinning={this.state.loading}>
-                <div className='card'>
-                    {
-                        (this.props.isNotice ? this.state.courseIsApply : this.state.courseNotApply).map((item, i) => {
-                            return (
-                                <div className='card_pad'>
-                                    <Card
-                                        title={item.CourseName}
-                                        extra={
-                                            this.state.isNotice ?
-                                                (
-                                                    <Icon
-                                                        type="bell"
-                                                        theme='filled'
-                                                        style={{fontSize:20}}
-                                                    // onClick={
-                                                    //     () => {
-                                                    //         this.setState({ modalVisible: true })
-                                                    //     }
-                                                    // }
-                                                    />
-                                                ) : (
-                                                    <Popconfirm
-                                                        title="是否立即开始通知?"
-                                                        onConfirm={() => {
-                                                            this.getCurrentCourseIsApply(item)
-                                                            this.setState({ modalVisible: true })
-                                                        }}
-                                                        okText="Yes"
-                                                        cancelText="No"
-                                                    >
+            <Spin spinning={this.props.infor.loading}>
+                <Checkbox.Group style={{ width: '100%' }}>
+                    <div className='card'>
+                        {
+                            (this.props.isNotice ? this.props.infor.courseIsApply : this.props.infor.courseNotApply).map((item, i) => {
+                                return (
+                                    <div className='card_pad'>
+                                        <Card
+                                            title={
+                                                !this.props.isNotice ?
+                                                    // <Checkbox>{item.CourseName}</Checkbox>
+                                                    item.CourseName
+                                                    : item.CourseName
+                                            }
+                                            extra={
+                                                this.props.isNotice ?
+                                                    (
                                                         <Icon
                                                             type="bell"
-                                                            theme='outlined'
-                                                            style={{fontSize:20}}
+                                                            theme='filled'
+                                                            style={{ fontSize: 20 }}
+                                                        // onClick={
+                                                        //     () => {
+                                                        //         this.setState({ modalVisible: true })
+                                                        //     }
+                                                        // }
                                                         />
-                                                    </Popconfirm>
-                                                )
-                                        }
+                                                    ) : (
+                                                        <Popconfirm
+                                                            title="是否立即开始通知?"
+                                                            onConfirm={() => {
+                                                                this.getCurrentCourseIsApply(item,i)
 
-                                        key={i}
-                                    >
-                                        <div className='card_container'>
-                                            <div className='card_item'>主讲人:{item.Teacher}</div>
-                                            <div className='card_item'>课程报名人数:{item.Attendees}</div>
-                                            <div className='card_item'>地点:{item.CourseLocation}</div>
-                                            <div className='card_item'>时间:{item.StartDatetime}</div>
+                                                            }}
+                                                            okText="Yes"
+                                                            cancelText="No"
+                                                        >
+                                                            <Icon
+                                                                type="bell"
+                                                                theme='outlined'
+                                                                style={{ fontSize: 20 }}
+                                                            />
+                                                        </Popconfirm>
+                                                    )
+                                            }
+
+                                            key={i}
+                                        >
+                                            <div className='card_container'>
+                                                <div className='card_item'>主讲人:{item.Teacher}</div>
+                                                <div className='card_item'>课程报名人数:{item.Attendees}</div>
+                                                <div className='card_item'>地点:{item.CourseLocation}</div>
+                                                <div className='card_item'>时间:{item.StartDatetime}</div>
+                                            </div>
+                                        </Card>
+                                    </div>
+                                )
+                            })
+                        }
+                        <Modal
+                            title="通知"
+                            visible={this.state.modalVisible}
+                            onOk={this.handleOk}
+                            onCancel={this.handleCancel}
+                        >
+                            <div style={{ display: 'flex', justifyContent: 'center' }} >
+                                <Progress style={{ width: 250 }} percent={this.state.percent} />
+                            </div>
+                            {
+                                this.state.peopleForNotApply.length ? (
+                                    <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+                                        <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', marginTop: 10, marginBottom: 10 }} >
+                                            {
+                                                this.state.peopleForNotApply.map((item) => {
+                                                    return (
+                                                        < div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: 100, margin: '5' }}>
+                                                            {item.C3_613941385305}
+                                                            <Icon type="close-circle" style={{ color: 'red' }} />
+                                                        </div>
+                                                    )
+                                                })
+                                            }
                                         </div>
-                                    </Card>
-                                </div>
-                            )
-                        })
-                    }
-                    <Modal
-                        title="通知"
-                        visible={this.state.modalVisible}
-                        onOk={this.handleOk}
-                        onCancel={this.handleCancel}
-                    >
-                        <div style={{ display: 'flex', justifyContent: 'center' }} >
-                            <Progress style={{ width: 250 }} percent={this.state.percent} />
-                        </div>
-                        {
-                            this.state.peopleForNotApply.length ? (
-                                <div style={{ display: 'flex',alignItems: 'center', flexDirection: 'column' }}>
-                                    <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', marginTop: 10, marginBottom: 10 }} >
                                         {
-                                            this.state.peopleForNotApply.map((item) => {
-                                                return (
-                                                    < div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: 100, margin: '5' }}>
-                                                        {item.C3_613941385305}
-                                                        <Icon type="close-circle" style={{ color: 'red' }} />
-                                                    </div>
-                                                )
-                                            })
+                                            (
+                                                this.state.peopleForNotApply.length ||
+                                                this.state.peopleNotApply.length
+                                            ) +
+                                                this.state.peopleIsApply.length ==
+                                                this.state.currentCourseAllPeople.length ? (
+                                                    < Button onClick={() => {
+                                                        this.setState({
+                                                            peopleNotApply: [...this.state.peopleForNotApply],
+                                                            peopleForNotApply: []
+                                                        }, () => {
+                                                            this.modifyCurrentCourseIsApply()
+                                                        })
+                                                    }}> 继续通知</Button>
+                                                ) : (null)
                                         }
                                     </div>
-                                    {
-                                        (
-                                            this.state.peopleForNotApply.length ||
-                                            this.state.peopleNotApply.length
-                                        ) +
-                                            this.state.peopleIsApply.length ==
-                                            this.state.currentCourseAllPeople.length ? (
-                                                < Button onClick={() => {
-                                                    this.setState({
-                                                        peopleNotApply: [...this.state.peopleForNotApply],
-                                                        peopleForNotApply: []
-                                                    }, () => {
-                                                        this.modifyCurrentCourseIsApply()
-                                                    })
-                                                }}> 继续通知</Button>
-                                            ) : (null)
-                                    }
-                                </div>
-                            ) : (null)
-                        }
-                    </Modal>
-                </div>
+                                ) : (null)
+                            }
+                        </Modal>
+                    </div>
+                </Checkbox.Group>
             </Spin>
         )
     }
--- src/components/custom/ExternalTraining/ETnoticeMain/ETnoticeMain.js
+++ src/components/custom/ExternalTraining/ETnoticeMain/ETnoticeMain.js
@@ -1,25 +1,186 @@
 import React from 'react';
 import ETNoticed from './ETNoticed'
-import { Skeleton, Switch, Card, Icon, Avatar, Radio, Tabs } from 'antd';
+import { Input, Tabs, Button, DatePicker, Checkbox, Modal, message, notification, Icon } from 'antd';
 import './ETnoticeMain.less';
+import http from 'Util20/api';
 
+
+const Search = Input.Search;
 class ETnoticeMain extends React.Component {
+    state = {
+        key: 0, //当前是哪个tab页
+        courseArrangment: [],
+        courseIsApply: [], //已经通知过的课程
+        courseNotApply: [], //未通知过的课程
+        loading: false,
+        searchText: '',
+        isShowModal: false
+    }
+    componentDidMount = () => {
+        this.setState({ loading: true })
+        this.getCourseArrangment()
+    }
+    getCourseArrangment = async () => {
+        let res;
+        let normalData = {
+            resid: '613959525708',
+        }
+        try {
+            res = await http().getTable(normalData);
+        } catch (error) {
+            return console.log(error);
+        }
+        if (res.error === 0) {
+            let courseArrangment = res.data;
+            let courseIsApply = []
+            let courseNotApply = []
+            courseArrangment.forEach((item) => {
+                if (item.C3_614256491795 == 'Y') {
+                    courseIsApply.push(item)
+                } else {
+                    courseNotApply.push(item)
+                }
+            })
+            this.setState({ courseArrangment, courseIsApply, courseNotApply, loading: false });
+        }
+    };
+    getCourseForTab = async (e) => {
+        let res;
+        try {
+            res = await http().getTable({
+                resid: '613959525708',
+                cmswhere: this.state.key ? "C3_614256491795 = 'Y'" : `C3_614256491795 IS NULL `,
+                key: e
+            });
+        } catch (error) {
+            return console.log(error);
+        }
+        if (res.error === 0) {
+            let courseIsApply = []
+            let courseNotApply = []
+            if (this.state.key) {
+                courseIsApply = res.data
+                this.setState({
+                    courseIsApply
+                })
+            } else {
+                courseNotApply = res.data
+                this.setState({
+                    courseNotApply
+                })
+            }
+            this.setState({ loading: false });
+        }
+    }
+    tabsChange = (e) => {
+        console.log(e)
+        this.setState({ key: e })
+    }
 
+    startRunAutoApply = async () => {//开始进行全部通知任务
+        let id = 613848452461
+        let res;
+        try {
+            res = await http().runAutoImport({
+                id
+            });
+        } catch (err) {
+            console.error(err);
+            message.error('正在通知，请耐心等候');
+        }
+    }
+    getTaskInfo = async () => {//查看任务进度
+        let res = {
+            error: -1
+        };
+        try {
+            res = await http().getAutoImportStatus();
+        } catch (err) {
+            message.error(err.message);
+        }
+        if (res.error !== 0) {
+            setTimeout(async () => {
+                await this.getTaskInfo()
+            }, 1000)
+            message.error(res.message);
+        } else {
+            notification.open({
+                message: '通知',
+                description: '人员通知完毕！',
+                icon: <Icon type="smile" style={{ color: '#108ee9' }} />,
+                duration: null
+            });
+        }
+    }
+    handleOk = e => {
+        this.setState({
+            isShowModal: false,
+        });
+    };
 
-
+    handleCancel = e => {
+        this.setState({
+            isShowModal: false,
+        });
+    };
+    onCurrentCourseIsApply(e, i) {
+        console.log(e, i)
+        let courseIsApply = [...this.courseIsApply]
+        let courseNotApply = [...this.courseNotApply]
+        courseIsApply.splice(i, 1)
+        courseNotApply.push(e)
+    }
     render() {
         const { TabPane } = Tabs;
         return (
             <div className='et_main'>
-                <Tabs defaultActiveKey="1">
-                    <TabPane tab="未通知" key="1">
-                        <ETNoticed isNotice={0}/>
+                <Tabs
+                    defaultActiveKey={this.state.key}
+                    onChange={this.tabsChange}
+                    tabBarExtraContent={
+                        <div>
+                            {/* <Checkbox>全选</Checkbox> */}
+                            {
+                                !this.state.key ?
+                                    <Button
+                                        style={{ marginRight: 10 }}
+                                        onClick={() => {
+                                            this.setState({
+                                                isShowModal: true
+                                            })
+                                        }}
+                                    >全部通知</Button>
+                                    : null
+                            }
+                            <DatePicker.RangePicker style={{ marginRight: 10 }} onChange={() => {
+
+                            }} />
+                            <Search
+                                placeholder="搜索"
+                                onSearch={value => this.setState({
+                                    loading: true
+                                }, () => this.getCourseForTab(value))}
+                                style={{ width: 200, marginRight: 10 }}
+                            />
+                        </div>
+                    }>
+                    <TabPane tab="未通知" key={0}>
+                        <ETNoticed infor={this.state} currentCourseIsApply={this.onCurrentCourseIsApply} isNotice={0} />
                     </TabPane>
-                    <TabPane tab="已通知" key="2">
-                        <ETNoticed isNotice={1}/>
+                    <TabPane tab="已通知" key={1}>
+                        <ETNoticed infor={this.state} isNotice={1} />
                     </TabPane>
 
                 </Tabs>
+                <Modal
+                    title="通知"
+                    visible={this.state.isShowModal}
+                    onOk={this.handleOk}
+                    onCancel={this.handleCancel}
+                >
+
+
+                </Modal>
             </div>
         )
     }
