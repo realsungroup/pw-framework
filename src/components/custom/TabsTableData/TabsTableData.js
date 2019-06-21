49d93da28c08cccf22c62b15964d28d783538089
Mac <mac@MacdeMacBook-Pro.local>
2019-06-21 18:01:22 +0800

    更羞tabs组建

--- src/components/custom/TabsTableData/TabsTableData.js
+++ src/components/custom/TabsTableData/TabsTableData.js
@@ -13,9 +13,16 @@ class TabsTableData extends React.Component {
     super(props);
   }
   show = () => {
-   return  this.props[0]&&this.props[0].map((prop,index) => {
+    let arr = []
+    if(this.props[0].length>0){
+      arr = this.props[0]
+    }else if(this.props.arr.length>0){
+      arr = this.props.arr
+    }
+   return  arr&&arr.map((prop,index) => {
      return ( 
      <TabPane
+        
         tab={prop.TabsTitle}
         key={index+1}
         style={{ width: '100%', height: '100%' }}
