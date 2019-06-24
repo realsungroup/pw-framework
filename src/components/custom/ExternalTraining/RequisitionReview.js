a05dd3f2bb6dbab3c6e692a1ab203068bc666b0d
dengming <13767267543@163.com>
2019-06-24 10:53:06 +0800

    使用tabstabledata组件

--- src/components/custom/ExternalTraining/RequisitionReview.js
+++ src/components/custom/ExternalTraining/RequisitionReview.js
@@ -1,17 +1,81 @@
 import React from 'react';
 import { TableData } from '../../common/loadableCommon';
+import TabsTableData from '../TabsTableData';
 
 function ReviewRequisition(props) {
   return (
-    <div style={{flex:1}}>
-      <TableData
-        resid="614184482830"
-        height='100%'
-        subtractH={200}
-        hasRowView={false}
-        hasModify={false}
-        hasDelete={false}
-        hasRowSelection={true}
+    <div style={{ flex: 1, display: 'flex'}}>
+      <TabsTableData
+        arr={[
+          {
+            resid: 614449812342,
+            TabsTitle: '待审核',
+            recordFormFormWidth: '90%',
+            hasBeBtns: true,
+            hasModify: false,
+            hasDelete: false,
+            hasAdd: false,
+            hasRowDelete: false,
+            hasRowModify: false,
+            hasRowView: true,
+            subtractH: 220,
+            recordFormType: 'drawer',
+            recordFormContainerProps: {
+              placement: 'bottom',
+              height: 600
+            },
+            subTableArrProps: [
+              {
+                subTableName: '计划详情',
+                subResid: 611315248461,
+                tableProps: {
+                  hasAdd: false,
+                  hasModify: false,
+                  hasDelete: false,
+                  hasRowModify: false,
+                  hasRowView: true,
+                  hasRowDelete: false,
+                  height: 400,
+                  subtractH: 196
+                }
+              }
+            ]
+          },
+          {
+            resid: 614449831159,
+            TabsTitle: '已审核',
+            recordFormFormWidth: '90%',
+            hasBeBtns: true,
+            hasModify: false,
+            hasDelete: false,
+            hasAdd: false,
+            hasRowDelete: false,
+            hasRowModify: false,
+            hasRowView: false,
+            subtractH: 220,
+            recordFormType: 'drawer',
+            recordFormContainerProps: {
+              placement: 'bottom',
+              height: 600
+            },
+            subTableArrProps: [
+              {
+                subTableName: '计划详情',
+                subResid: 611315248461,
+                tableProps: {
+                  hasAdd: false,
+                  hasModify: false,
+                  hasDelete: false,
+                  hasRowModify: false,
+                  hasRowView: true,
+                  hasRowDelete: false,
+                  height: 400,
+                  subtractH: 196
+                }
+              }
+            ]
+          }
+        ]}
       />
     </div>
   );
