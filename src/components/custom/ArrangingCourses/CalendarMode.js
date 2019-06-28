2f954de3760bc84b1d3e9954c7f6bedf2d2f7ab5
Mac <mac@MacdeMacBook-Pro.local>
2019-06-28 17:15:52 +0800

    合并

--- src/components/custom/ArrangingCourses/CalendarMode.js
+++ src/components/custom/ArrangingCourses/CalendarMode.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import Calendar from 'ic-components/lib/Calendar';
-// import 'ic-components/lib/Calendar/style/index.less';
+// import React from 'react';
+// import Calendar from 'ic-components/lib/Calendar';
+// // import 'ic-components/lib/Calendar/style/index.less';
 
-class CalendarMode extends React.Component{
-    render(){
-        return <Calendar></Calendar>
-    }
-}
+// class CalendarMode extends React.Component{
+//     render(){
+//         return <Calendar></Calendar>
+//     }
+// }
 
-export default CalendarMode
+// export default CalendarMode
