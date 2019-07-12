d1ef79acbe97b95fbd04977c92f04ba676bbb9b9
dengming <13767267543@163.com>
2019-07-12 15:32:15 +0800

    二级部门经理查阅计划

---
+++ src/components/custom/ViewPlan/ReviewApplicationForm.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import TabsTableData from '../TabsTableData';
+
+const ReviewApplicationForm = props => {
+  return (
+    <div style={{ flex: 1, display: 'flex' }}>
+      <TabsTableData
+        arr={[
+          {
+            resid: 614449770755,
+            TabsTitle: '待审核',
+            OutHeight: '80vh',
+            recordFormFormWidth: '90%',
+            hasBeBtns: true,
+            hasModify: false,
+            hasDelete: false,
+            hasAdd: false,
+            actionBarWidth:250,
+            hasRowDelete: false,
+            hasRowModify: false,
+            hasRowView: true,
+            subtractH: 220,
+            recordFormType: 'drawer',
+            recordFormContainerProps: {
+              placement: 'bottom',
+              height: 600
+            }
+          },
+          {
+            resid: 614449790350,
+            TabsTitle: '已审核',
+            OutHeight: '80vh',
+            recordFormFormWidth: '90%',
+            hasBeBtns: true,
+            hasModify: false,
+            hasDelete: false,
+            hasAdd: false,
+            hasRowDelete: false,
+            hasRowModify: false,
+            hasRowView: false,
+            subtractH: 220,
+            recordFormType: 'drawer',
+            recordFormContainerProps: {
+              placement: 'bottom',
+              height: 600
+            }
+          }
+        ]}
+      />
+    </div>
+  );
+};
+
+export default ReviewApplicationForm;
---
+++ src/components/custom/ViewPlan/ViewTips.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import {
+  Button,
+  message,
+  Modal,
+	Divider,
+	Empty,
+  Card,
+  Row,
+  Col,
+  Rate,
+  DatePicker,
+  InputNumber
+} from 'antd';
+import moment from 'moment';
+import { TableData } from '../../common/loadableCommon';
+import http from 'Util20/api';
+
+const CourseArrangementDetailResid = '616245136209';
+const TIPS_RESID = '614964195659'; //心得表id
+
+class ViewTips extends React.Component {
+  state = {
+    viewActionsVisible: false,
+    selectedCourseArrangmentDetail: {},
+    tips: {}
+  };
+
+  getTips = async () => {
+    const { selectedCourseArrangmentDetail } = this.state;
+    try {
+      let res = await http().getTable({
+        resid: TIPS_RESID,
+        cmswhere: `C3_615479417558 = ${selectedCourseArrangmentDetail.CourseArrangeDetailID}`
+      });
+      if (res.data.length) {
+        this.setState({ tips: { ...res.data[0] } });
+      }
+    } catch (error) {
+      message.error(error.message);
+      console.log(error.message);
+    }
+  };
+
+  //关闭模态窗
+  handleCloseModal = () =>
+    this.setState({
+      viewActionsVisible: false,
+      selectedCourseArrangmentDetail: {},
+      tips: {}
+    });
+
+  render() {
+    let { tips } = this.state;
+    return (
+      <div style={{ flex: 1, height: '100%' }}>
+        <TableData
+          resid={CourseArrangementDetailResid}
+          subtractH={240}
+          hasRowView={false}
+          hasAdd={false}
+          hasModify={false}
+          hasDelete={false}
+          hasRowSelection={true}
+          hasRowDelete={false}
+          hasRowModify={false}
+					actionBarWidth={150}
+          customRowBtns={[
+            record => {
+              return (
+                <Button
+                  onClick={() => {
+                    this.setState(
+                      {
+                        viewActionsVisible: true,
+                        selectedCourseArrangmentDetail: record
+                      },
+                      this.getTips
+                    );
+                  }}
+                >
+                  查看心得
+                </Button>
+              );
+            }
+          ]}
+          cmswhere={`courseType <> '外聘内训'`}
+        />
+        <Modal
+          title="查看心得"
+          visible={this.state.viewActionsVisible}
+          onCancel={this.handleCloseModal}
+          onOk={this.handleCloseModal}
+          width="70%"
+          destroyOnClose
+        >
+          {tips.C3_614964225030?<div style={{ padding: 12 }}>
+            {/* 标题 */}
+            <h2 style={{ textAlign: 'center' }}>{tips.C3_614964239022}</h2>
+            <Divider />
+            {/* 内容 */}
+            <pre>{tips.C3_614964225030}</pre>
+          </div>: <Empty description='未提交心得'></Empty>}
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default ViewTips;
