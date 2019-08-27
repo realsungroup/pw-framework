ecadc31d4b94206908cc75762649bf741c5ec298
dengming <13767267543@163.com>
2019-08-27 09:58:32 +0800

    增加state

--- src/components/custom/ComprehensiveQuery/ComprehensiveQuery.js
+++ src/components/custom/ComprehensiveQuery/ComprehensiveQuery.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import TreeRel from '../../common/ui/TreeRel';
 import './ComprehensiveQuery.less';
 
 class ComprehensiveQuery extends React.Component {
+  state = {
+    node: []
+  };
+  setSelect = node => {
+    this.setState({
+      node
+    });
+  };
   render() {
-    return null;
+    return (
+      <div id="comprehensive-query">
+        <TreeRel
+          url="api/OrgChart/GetNodesData"
+          resid="602348115218"
+          ColumnOfID="C3_602347243263"
+          ColumnOfPID="C3_602347244770"
+          ProductIDs="1360564"
+          autoExpandParent="true"
+          onSelect={this.setSelect}
+        />
+        <div
+          style={{
+            height: 500,
+            backgroundColor: '#fff',
+            position: 'fixed',
+            top: 0,
+            // left: 'calc(100% - )',
+            right: 0
+          }}
+        >
+          asfd
+        </div>
+      </div>
+    );
   }
 }
 
