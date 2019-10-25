af197aa4174073f7846959118b9d1f04d02c3368
hileix <304192604@qq.com>
2019-10-25 08:44:16 +0800

    subcourse

--- src/components/custom/SubordinateCourses/SubordinateCourses.js
+++ src/components/custom/SubordinateCourses/SubordinateCourses.js
@@ -201,7 +201,19 @@ class SubordinateCourses extends React.Component {
       message.error(error.message);
     }
   };
-
+  handleReturn= async =>{
+    console.log()
+    // let res; //课程反馈
+    // try {
+    //   res = await http().getTable({
+    //     resid: 478367996508,
+    //     cmswhere: `C3_478368118696 =${courseArrangmentDetailToSearch.CourseArrangeDetailID}`
+    //   });
+    // } catch (err) {
+    //   console.log(err.message);
+    //   message.error(err.message);
+    // }
+  }
   handleOk = () => {
     this.setState({
       courseDetailVisible: false,
@@ -552,6 +564,7 @@ class SubordinateCourses extends React.Component {
           hasAdd={false}
           // hasRowSelection={true}
           customRowBtns={[
+
             record => {
               return (
                 <Button
@@ -787,6 +800,7 @@ class SubordinateCourses extends React.Component {
               })}
             </Card>
           )}
+          <Button style={{position:'absolute',bottom:'16px'}} onClick={this.handleReturn}>退回这个课程反馈</Button>
         </Drawer>
         <Drawer
           width={'60%'}
