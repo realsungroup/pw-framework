081c6a22975ba81ccce4d1a7d672cd94e93db622
tao han <hantao@realsun.biz>
2019-11-11 10:39:10 +0800

    xx

--- public/functions.config.js
+++ public/functions.config.js
@@ -4,7 +4,7 @@ window[588425594397] = {
   name: 'PatientInfo',
   props: {
     tableDataProps: {
-      resid: 586639160406,
+      resid: 626547652750,
       subtractH: 100,
       actionBarFixed: false,
       height: 500,
@@ -22,3 +22,29 @@ window[588425594397] = {
     }
   }
 };
+
+
+window[626543183115] = {
+  title: '生物样本库',
+  name: 'PatientInfo',
+  props: {
+    tableDataProps: {
+      resid:626550360141,
+      subtractH: 100,
+      actionBarFixed: false,
+      height: 500,
+      size: 'small',
+      actionBarWidth: 490,
+      hasModify: false,
+      hasDelete: false,
+      addText: '添加样本信息',
+      enAddText: 'Add basic information',
+      rowModifyText: '修改样本信息',
+      enRowModifyText: 'Modify basic information',
+      formProps: {
+        displayMode: 'classify'
+      }
+    }
+  }
+};
+
