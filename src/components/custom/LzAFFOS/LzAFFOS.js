15f856a451d4b0834ea60d415f8dbb67708b32a4
codetanqi <1528992674@qq.com>
2020-01-06 09:27:27 +0800

    访客申请按钮更改名称

--- src/components/custom/LzAFFOS/LzAFFOS.js
+++ src/components/custom/LzAFFOS/LzAFFOS.js
@@ -131,7 +131,7 @@ export default class LzAFFOS extends React.Component {
                 wrappedComponentRef={element => (this.tableDataRef = element)}
                 refTargetComponentName="TableData"
                 formProps={{
-                  saveText: '请添加访客信息后再提交',
+                  saveText: '请添加访客信息后再保存',
                   height: 500,
                   saveNeedConfirm: true
                 }}
