f0a104b852da3fd45a2ebfe86d91ed5c94722a50
hileix <304192604@qq.com>
2020-01-15 20:18:27 +0800

    idltransfer

--- src/components/custom/IDLTransfer/IDLTransfer.js
+++ src/components/custom/IDLTransfer/IDLTransfer.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import {Steps,Button,Icon,Select,Input,Tabs,Spin,TreeSelect} from 'antd';
+import {Steps,Button,Icon,Select,Input,Tabs,Spin,TreeSelect,Modal, DatePicker} from 'antd';
 import './IDLTransfer.less';
 import TableData from '../../common/data/TableData';
 import http from 'Util20/api';
 
 const { Option } = Select;
 const { TabPane } = Tabs;
-
 const { Step } = Steps;
  class  IDLTransfer extends  Component{
    constructor(props){
     super(props);
     this.state ={
+      depaOrg:null,//部门的原始数据
       step:0,//申请步骤
       selectedRecord:[],//选中的人
       hint:'关于张三的转岗申请已经提交成功。',//提交结果提示
@@ -24,14 +24,51 @@ const { Step } = Steps;
       loading:false,
       department:[],//部门数据,
       company:[{name:'无锡',value:'100'},{name:'上海',value:'2000'}],//公司数据，
-      companyV:'100',
-      depaV:null
+      companyV:'100',//选中的公司值
+      depaV:null,//选中的部门值
+      selectMem:[],//选中的人员
+      searchDepaV:false,//搜索部门模态框是否可见
+      searchSuperV:false,//搜索主管模态框是否可见
+      newDepa:{},//新部门信息
+      newSuper:{},//新主管信息
+      job:'',//新职位
+      searchJobV:false,//搜索职位模态框是否可见
+      lvList:[],//级别下拉选择列表
+      lv:'',//级别
+      proId:'',//项目代码
+      bucode:'',
+      changeType:'',//变更类型
+      activeDate:'',//生效日期
+      depaSele:{},//原部门的一级部门二级部门三级部门四级部门的数据
+      toSub:{
+
+      }//要提交的数据
     };
    }
    componentDidMount(){
+     this.getLv();
      this.getDepartment('100');
    }
-   
+   getLv=async()=>{
+     this.setState({loading:true});
+    var res='';
+    try{
+      res = await http().getTable({
+        resid: 449335746776,
+      });
+      var n=0;
+      var arr=[];
+      while (n<res.data.length){
+        arr.push({value:res.data[n].C3_449335790387,key:res.data[n].C3_449335790387})
+        n++;
+      }
+      this.setState({lvList:arr,loading:false});
+    }catch(e){
+      this.setState({loading:false})
+      console.log(e)
+    }
+   }
+  //  获取部门的树的数据并整理数据
    getDepartment = async(v)=>{
      this.setState({department:[],loading:true});
     var res='';
@@ -40,6 +77,8 @@ const { Step } = Steps;
         resid: 632327119162,
         cmswhere:`C3_419448448137 = '${v}'`
       });
+      this.setState({depaOrg:res.data})
+      console.log(res.data)
       var n=0;
       var obj=res.data;
       var arr=[];
@@ -169,19 +208,44 @@ const { Step } = Steps;
       this.setState({loading:false});
       console.log(e)}
    }
+  // 第一页切换部门
    onChange = value => {
-    console.log(value);
-    this.setState({ depaV:value });
+    this.setState({ depaV:value ,selectMem:[]});
   };
+  // 第一页切换公司
   handleChange = (v) =>{
     this.setState({companyV:v,depaV:null});
     this.getDepartment(v);
   }
+  // 向后台提交数据
    subData = async() =>{
       this.setState({step:2,isSub:true})
    }
+  //  翻tab
    callBack = (k) =>{
     this.setState({page:k,isSub:false})
+   }
+  //  第一页跳转第二页
+   subMemData = (dataSource, selectedRowKeys) =>{
+    var data=[];
+    dataSource.map(item => {
+      if (selectedRowKeys.includes(item.REC_ID)) {
+        data.push(item);
+      }
+    });
+    var data2=[]
+    // 存部门的第一部门第二部门第三部门第四部门
+    var arr=[this.state.depaV];
+    this.state.depaOrg.map(item => {
+      if (arr.includes(item.DEP_ID)) {
+        data.push(item);
+      }
+    });console.log(data2)
+    this.setState({selectMem:data,step:1,depaSele:data2})
+   }
+  //  关闭部门查询模态框
+   clzDepaSearch=()=>{
+     this.setState({searchDepaV:false})
    }
    render(){
      return (
@@ -197,7 +261,7 @@ const { Step } = Steps;
             {
               this.state.step==0?(
               <Spin style={{width:'100%',height:'100%'}}spinning={this.state.loading}>
-              <div className='sider focusWindow' style={{width:'200px',paddingRight:'24px',marginTop:'24px',height:'calc(100vh - 132px)'}}>
+              <div className={this.state.depaV?'sider load':'load sider focusWindow'} style={{width:'200px',paddingRight:'24px',marginTop:'24px',height:'calc(100vh - 132px)'}}>
                 请选择公司:
                 <Select value={this.state.companyV} onChange={(v)=>this.handleChange(v)} defaultValue='100' style={{ width: '100%',marginBottom:'8px',marginTop:'8px' }}>
                   {
@@ -216,22 +280,246 @@ const { Step } = Steps;
                   placeholder="请先选择部门"
                   showSearch={true}
                   searchPlaceholder='输入部门编号搜索'
-                  // treeDefaultExpandAll
                   onChange={this.onChange}
                 />
               </div>
-              <footer>
-                <Button type='primary' onClick={()=>this.setState({step:1})}>下一步</Button>
-              </footer>
               </Spin>
               ):null
             }
             {
-              this.state.step==1?(<footer>
-                <Button onClick={()=>this.setState({step:0})}>上一步</Button>
-                <Button style={this.state.isSub?{}:{display:'none'}} type='primary' onClick={()=>this.setState({step:2})}>下一步</Button>
-                <Button style={!this.state.isSub?{}:{display:'none'}} type='primary' onClick={()=>this.subData()}>提交</Button>
-              </footer>):null
+              this.state.step==0&&this.state.depaV?
+              (
+                <div className='load' style={{float:'left',width:'calc(100% - 224px)',marginLeft:'24px',marginTop:'24px',height:'calc(100% - 64px)'}}>
+                  <TableData
+                    resid={609599795438}
+                    hasRowView={false}
+                    hasAdd={false}
+                    cmswhere={`HRUSER_DEPID = '${this.state.depaV}'`}
+                    hasRowSelection={false}
+                    hasRowDelete={false}
+                    hasRowModify={false}
+                    hasModify={false}
+                    hasDelete={false}
+                    style={{ height: '100%'}}
+                    hasRowView={false}
+                    hasRowSelection={true}
+                    actionBarExtra={({ dataSource, selectedRowKeys }) => {
+                      return (
+                        <Button type='primary' disabled={!(selectedRowKeys.length>0)} onClick={()=>{this.subMemData(dataSource,selectedRowKeys)}}>下一步</Button>
+                      );
+                    }}
+                  />
+                </div>
+              )
+              :null
+            }
+            {
+              this.state.step==1?(
+              <div style={{width:'100%',height:'calc(100vh - 156px)',position:'relative'}} className='load'>
+                
+                <div className='form'>
+                  
+                  <h3>变更后：</h3>
+                  <b>生效日期：</b>
+                  <span style={{width:'auto'}}>
+
+                  <DatePicker
+                    value={this.state.activeDate}
+                    onChange={(v)=>this.setState({activeDate:v})}
+                  />
+                  </span>
+                  <br/>
+                  <br/>
+                  <b>变更类型：</b>
+                  <span style={{width:'auto'}}>
+                  <Select placeholder='请选择变更类型' style={{ width: 240 }} onChange={(v)=>{this.setState({changeType:v})}}>
+
+                    <Option value='汇报关系' key='0'>汇报关系</Option>
+                    <Option value='部门变更' key='1'>部门变更</Option>
+                    <Option value='部门变更' key='2'>部门变更</Option>
+                    <Option value='部门变更' key='3'>部门变更</Option>
+
+
+                  </Select>
+                  </span>
+                  <br/>
+                  <br/>
+              <b style={this.state.newDepa.C3_419339113187?{}:{color:'#f5222d'}}>变更后部门代码：</b><span style={{width:'248px',marginRight:'16px',minWidth:'0'}}>{this.state.newDepa.C3_419339113187?this.state.newDepa.C3_419339113187:<span style={{color:'#f5222d'}}>请点击右侧按钮选择部门</span>}</span>
+                  <Button icon="search" onClick={()=>this.setState({searchDepaV:true})}>选择部门</Button>
+                  <div>
+                  <b>变更后部门名：</b><span>{this.state.newDepa.DEP_NAME?this.state.newDepa.DEP_NAME:'- -'}</span>
+                  <b>变更后部门英文名：</b><span>{this.state.newDepa.DEP_NAME_EN?this.state.newDepa.DEP_NAME_EN:'- -'}</span>
+                  <b>变更后一级部门名：</b><span>{this.state.newDepa.C3_461011984661?this.state.newDepa.C3_461011984661:'- -' }</span>
+                  {/* <b>变更后一级部门代码：</b><span>{this.state.newDepa.DEP_NAME}</span> */}
+                  <b>变更后二级部门名：</b><span>{this.state.newDepa.C3_461011984896?this.state.newDepa.C3_461011984896:'- -' }</span>
+                  {/* <b>变更后二级部门代码：</b><span>{this.state.newDepa.DEP_NAME}</span>  */}
+                  <b>变更后三级部门名：</b><span>{this.state.newDepa.C3_461011985099?this.state.newDepa.C3_461011985099:'- -' }</span>
+                  {/* <b>变更后三级部门代码：</b><span>{this.state.newDepa.DEP_NAME}</span> */}
+                  <b>变更后四级部门名：</b><span>{this.state.newDepa.C3_461011985365?this.state.newDepa.C3_461011985365:'- -' }</span>
+                  {/* <b>变更后四级部门代码：</b><span>{this.state.newDepa.DEP_NAME}</span> */}
+                  </div>
+                  <b >变更后项目代码：</b>
+                  <Input value={this.state.proId} onChange={(v)=>{this.setState({proId:v.target.value})}}/>
+                  <br/>
+                  <br/>
+                  <b>变更后主管：</b><span style={{minWidth:'248px',marginRight:'16px',minWidth:'0'}}>{this.state.newSuper.C3_227192484125?(this.state.newSuper.C3_227192484125+' - '+this.state.newSuper.C3_305737857578):<span style={{color:'#f5222d'}}>请点击右侧按钮选择主管</span>}</span>
+                  <Button icon="search" onClick={()=>this.setState({searchSuperV:true})}>选择主管</Button>
+                  <br/>
+                  <br/>
+
+                  <b>变更后职位：</b>
+                  <span style={{minWidth:'248px',marginRight:'16px',minWidth:'0'}}>{this.state.job?(this.state.job.C3_602347243459+this.state.job.C3_612377399102+' - '+this.state.job.C3_602347243263):<span style={{color:'#f5222d'}}>请点击右侧按钮选择职位</span>}</span>
+                  <Button icon="search" onClick={()=>this.setState({searchJobV:true})}>选择职位</Button>
+                  
+                  <br/>
+                  <br/>
+
+                  <b>变更后级别：</b>
+                  <span style={{width:'auto'}}>
+                  <Select placeholder='请选择级别' style={{ width: 240 }} onChange={(v)=>{this.setState({lv:v})}}>
+                    {this.state.lvList.map((item)=>{return(
+                    <Option value={item.value} key={item.key}>{item.value}</Option>
+
+                    )})}
+                  </Select>
+                  </span>
+                  <br/>
+                  <br/>
+
+                  <b>bucode：</b>
+                  <Input value={this.state.bucode} onChange={(v)=>{this.setState({bucode:v.target.value})}}/>
+                </div>
+                <div className='memberList'>
+                  <h3>变更前部门：</h3>
+                  <div>
+                    {/* <b>一级部门：</b><span>{this.state.depaSele?this.state.depaSele[0].C3_461011945566:'- -'}</span>
+                    <b>二级部门：</b><span>{this.state.depaSele?this.state.depaSele[0].C3_461011984896:'- -'}</span>
+                    <b>三级部门：</b><span>{this.state.depaSele?this.state.depaSele[0].C3_461011985099:'- -'}</span>
+                    <b>四级部门：</b><span>{this.state.depaSele?this.state.depaSele[0].C3_461011985365:'- -'}</span> */}
+                  </div>
+                  <h3>待变更人员：<b>{this.state.selectMem.length}</b></h3>
+                  <ul>
+                    {this.state.selectMem.map((item,key)=>{return(
+                      <li>
+                      <h4>{key+1}</h4>
+                      <p>
+                      <b>姓名：</b><span>{item.C3_227192484125}</span>
+                      </p>
+                      <p>
+                      <b>英文名：</b><span>{item.C3_419343735913}</span>
+                      </p>
+                      <p><b>工号：</b><span>{item.C3_448032387764}</span></p>
+                      
+                      <p><b>当前主管：</b><span>{item.C3_417993433650}</span></p>
+                      
+                    <p><b>当前职位：</b><span>{item.C3_417990929305}</span></p>
+                      
+                      <p><b>当前级别：</b><span>{item.C3_587728697204}</span></p>
+                      
+                    </li>
+                    )})}
+                  </ul>
+                </div>
+                <Modal
+                  title="部门列表"
+                  visible={this.state.searchDepaV}
+                  footer={null}
+                  onCancel={this.clzDepaSearch}
+                  width={'80vw'}
+                  height={'80vh'}
+                >
+                  <div style={{width:'100%',height:'calc(80vh - 104px)',position:'relative'}}>
+                   <TableData
+                  resid={632327119162}
+                  cmswhere={`C3_419339113187 != ''`}
+                  hasRowView={false}
+                  subtractH={220}
+                  hasAdd={false}
+                  hasRowSelection={false}
+                  hasRowDelete={false}
+                  hasRowModify={false}
+                  hasModify={false}
+                  hasDelete={false}
+                  style={{ height: '100%'}}
+                  hasRowView={false}
+                  customRowBtns={[
+                    (record) => {
+                      return (
+                        <Button onClick={()=>{this.setState({newDepa:record,searchDepaV:false})}}>选择</Button>
+                      );
+                    }
+                  ]}
+                />
+                </div>
+                </Modal>
+                <Modal
+                  title="人员列表"
+                  visible={this.state.searchSuperV}
+                  footer={null}
+                  onCancel={()=>{this.setState({searchSuperV:false})}}
+                  width={'80vw'}
+                  height={'80vh'}
+                >
+                  <div style={{width:'100%',height:'calc(80vh - 104px)',position:'relative'}}>
+                   <TableData
+                  resid={609599795438}
+                  hasRowView={false}
+                  subtractH={220}
+                  hasAdd={false}
+                  hasRowSelection={false}
+                  hasRowDelete={false}
+                  hasRowModify={false}
+                  hasModify={false}
+                  hasDelete={false}
+                  style={{ height: '100%'}}
+                  hasRowView={false}
+                  customRowBtns={[
+                    (record) => {
+                      return (
+                        <Button onClick={()=>{this.setState({newSuper:record,searchSuperV:false})}}>选择</Button>
+                      );
+                    }
+                  ]}
+                />
+                </div>
+                </Modal>
+                <Modal
+                  title="职位列表"
+                  visible={this.state.searchJobV}
+                  footer={null}
+                  onCancel={()=>{this.setState({searchJobV:false})}}
+                  width={'80vw'}
+                  height={'80vh'}
+                >
+                  <div style={{width:'100%',height:'calc(80vh - 104px)',position:'relative'}}>
+                   <TableData
+                  resid={602348115218}
+                  hasRowView={false}
+                  subtractH={220}
+                  hasAdd={false}
+                  hasRowSelection={false}
+                  hasRowDelete={false}
+                  hasRowModify={false}
+                  hasModify={false}
+                  hasDelete={false}
+                  style={{ height: '100%'}}
+                  hasRowView={false}
+                  customRowBtns={[
+                    (record) => {
+                      return (
+                        <Button onClick={()=>{this.setState({job:record,searchJobV:false})}}>选择</Button>
+                      );
+                    }
+                  ]}
+                />
+                </div>
+                </Modal>
+                <footer>
+                  <Button onClick={()=>this.setState({step:0})}>上一步</Button>
+                  <Button style={this.state.isSub?{}:{display:'none'}} type='primary' onClick={()=>this.setState({step:2})}>下一步</Button>
+                  <Button style={!this.state.isSub?{}:{display:'none'}} type='primary' onClick={()=>this.subData()}>提交</Button>
+                </footer>
+              </div>):null
             }
             {
               this.state.step==2?(
