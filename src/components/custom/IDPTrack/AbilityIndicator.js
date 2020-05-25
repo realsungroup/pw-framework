a58dc81bc9830cbaf44c8fd34bfe94d0f4da9762
dengming <13767267543@163.com>
2020-05-25 17:00:06 +0800

    个人能力发展轨迹的能力指标为空时提示 当前财年无能力指标

--- src/components/custom/IDPTrack/AbilityIndicator.js
+++ src/components/custom/IDPTrack/AbilityIndicator.js
@@ -6,33 +6,46 @@ export default function AbilityIndicator(props) {
   const { currentYear } = props;
   // 没有数据的场合用默认数据代替
 
-  if(!currentYear.ability){
-   
-    var n=0;
-    var arr=[];
-    var j=currentYear.level;
-    while(n<abliLevel.data.length){
-      var c=0;
-      var bol=false;
-      while(c<abliLevel.data[n].chara.length){
-        if(j==abliLevel.data[n].chara[c].level){
-          if(props.isSupervisor==abliLevel.data[n].chara[c].isSupervisor){
-            bol=true;
+  if (!currentYear.ability) {
+    var n = 0;
+    var arr = [];
+    var j = currentYear.level;
+    while (n < abliLevel.data.length) {
+      var c = 0;
+      var bol = false;
+      while (c < abliLevel.data[n].chara.length) {
+        if (j == abliLevel.data[n].chara[c].level) {
+          if (props.isSupervisor == abliLevel.data[n].chara[c].isSupervisor) {
+            bol = true;
           }
         }
         c++;
       }
-      if(bol==true){
-        arr.push(abliLevel.data[n])
+      if (bol == true) {
+        arr.push(abliLevel.data[n]);
       }
       n++;
     }
-
-   }
+  }
+  if (arr) {
+    return arr.map((item, key) => (
+      <div className="IDPTrack__ability" key={key}>
+        <div className="IDPTrack__ability__header">
+          <div className="IDPTrack__ability__header__left">
+            <div>{item.en}</div>
+            <div>{item.ch}</div>
+          </div>
+          <div className="IDPTrack__ability__header__right">N/A</div>
+        </div>
+        <div className="IDPTrack__ability__measures">
+          <div className="IDPTrack__ability__measures__nomeasures">N/A</div>
+        </div>
+      </div>
+    ));
+  }
   return (
     <>
-      {currentYear &&
-        currentYear.ability &&
+      {currentYear && currentYear.ability ? (
         currentYear.ability.map(item => (
           <div className="IDPTrack__ability" key={item.REC_ID}>
             <div className="IDPTrack__ability__header">
@@ -68,29 +81,24 @@ export default function AbilityIndicator(props) {
               )}
             </div>
           </div>
-        ))}
-        {((currentYear&&currentYear.ability)||(arr))?'':("N/A")}
-        {
-          arr&&arr.map((item,key) =>(
-            <div className="IDPTrack__ability" key={key}>
-            <div className="IDPTrack__ability__header">
-              <div className="IDPTrack__ability__header__left">
-                <div>{item.en}</div>
-                <div>{item.ch}</div>
-              </div>
-              <div className="IDPTrack__ability__header__right">
-                N/A
-              </div>
-            </div>
-            <div className="IDPTrack__ability__measures">
-              
-                <div className="IDPTrack__ability__measures__nomeasures">
-                  N/A
-                </div>
-            </div>
-          </div>
-          ))
-        }
+        ))
+      ) : (
+        <div
+          style={{
+            fontSize: '1.5rem',
+            fontWeight: 'bold',
+            color: '#333',
+            height: '100%',
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+        >
+          当前财年无能力指标
+        </div>
+      )}
+      {(currentYear && currentYear.ability) || arr ? '' : 'N/A'}
     </>
   );
 }
