17f6993f94e3a597488d3b64065e4feadcb652d2
hileix <hileix@outlook.com>
2020-06-10 10:42:03 +0800

    fix:修复 TableData 在后端不显示删除按钮时，也会显示行删除按钮的 bug

--- src/components/common/data/TableData/TableData.js
+++ src/components/common/data/TableData/TableData.js
@@ -2069,30 +2069,31 @@ class TableData extends React.Component {
         } = this.props;
         let hasRowSaveCancel,
           hasRowBeBtns = hasBeBtns && !!beBtnsSingle.length,
-          hasCustomRowBtns = !!customRowBtns;
-
+          hasCustomRowBtns = !!customRowBtns,
+          _hasRowEdit = hasRowEdit,
+          _hasRowModify = hasRowModify && this._hasRowModify,
+          _hasRowDelete = hasRowDelete && this._hasRowDelete,
+          _hasRowView = hasRowView && this._hasRowView;
         if (hasRowEdit) {
           hasRowSaveCancel = record.REC_ID === editingKey;
-          hasRowEdit = hasRowEdit && !hasRowSaveCancel;
-          hasRowModify =
-            hasRowModify && !hasRowSaveCancel && this._hasRowModify;
-          hasRowView = hasRowView && !hasRowSaveCancel && this._hasRowView;
-          hasRowDelete =
-            hasRowDelete && !hasRowSaveCancel && this._hasRowDelete;
+          _hasRowEdit = hasRowEdit && !hasRowSaveCancel;
+          _hasRowModify = _hasRowModify && !hasRowSaveCancel;
+          _hasRowView = _hasRowView && !hasRowSaveCancel;
+          _hasRowDelete = _hasRowDelete && !hasRowSaveCancel;
           hasRowBeBtns = hasRowBeBtns && !hasRowSaveCancel;
           hasCustomRowBtns = hasCustomRowBtns && !hasRowSaveCancel;
         }
 
         return (
           <Fragment>
-            {hasRowEdit && this.renderRowEditBtn(record)}
+            {_hasRowEdit && this.renderRowEditBtn(record)}
 
             {hasRowSaveCancel && this.renderRowSaveBtn(record)}
             {hasRowSaveCancel && this.renderRowCancelBtn(record)}
 
-            {hasRowModify && this.renderRowModifyBtn(record)}
-            {hasRowView && this.renderRowViewBtn(record)}
-            {hasRowDelete && this.renderRowDeleteBtn(record)}
+            {_hasRowModify && this.renderRowModifyBtn(record)}
+            {_hasRowView && this.renderRowViewBtn(record)}
+            {_hasRowDelete && this.renderRowDeleteBtn(record)}
 
             {/* 后端按钮 */}
             {hasRowBeBtns && this.renderRowBeBtns(beBtnsSingle, record)}
