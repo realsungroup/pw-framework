df9474bce9cd328112f3d55b2cb55b5d0db18944
dengming <13767267543@163.com>
2020-07-28 18:33:20 +0800

    初步完成员工沟通管理员端

--- src/components/custom/StaffCommunicationRecord/StaffCommunicationRecord.js
+++ src/components/custom/StaffCommunicationRecord/StaffCommunicationRecord.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './StaffCommunicationRecord.less';
-import { Modal, Button, message, Tabs, Popconfirm, Input, Form } from 'antd';
+import { Tabs } from 'antd';
 // import http from '../../../../util20/api';
-import StaffComplain from './StaffComplain'
+import StaffComplain from './StaffComplain';
+import HelpAppeal from './HelpAndAppeal';
+import Advice from './Advice';
+
 const TabPane = Tabs.TabPane;
 
 class StaffCommunicationRecord extends React.Component {
@@ -15,28 +18,23 @@ class StaffCommunicationRecord extends React.Component {
   }
   state = {
     SquareCardArr: [],
-    val: null,
+    val: null
   };
-  
+
   render() {
     return (
-      <div className = 'container-box'>
-        <Tabs 
-        defaultActiveKey = "1"
-        className= 'tabs_container'
-        >
+      <div className="container-box">
+        <Tabs defaultActiveKey="1" className="tabs_container">
           <TabPane tab="投诉" key="1">
-          <StaffComplain />
+            <StaffComplain />
           </TabPane>
           <TabPane tab="求助或申诉" key="2">
-<div>13</div>
+            <HelpAppeal />
           </TabPane>
           <TabPane tab="合理化建议" key="3">
-
+            <Advice />
           </TabPane>
-          
         </Tabs>
-       
       </div>
     );
   }
