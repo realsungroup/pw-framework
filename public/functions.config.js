cdc69c488432ecd12bee7e3d88a48e85ca42e8d3
欧阳润林 <782572516@qq.com>
2020-08-03 18:10:29 +0800

    增加当前转诊列表模块

--- public/functions.config.js
+++ public/functions.config.js
@@ -5,15 +5,16 @@ window[648640265023] = {
   props: {
     tableDataProps: {
       resid: 648300111771,
+      isUseBESize: true,
       subtractH: 200,
       actionBarFixed: true,
-      hasDownload:false,
-      hasZoomInOut:true,
+      hasDownload: false,
+      hasZoomInOut: true,
       height: 500,
       afterSaveRefresh: true,
       hasAdd: false,
-      hasRowView:false,
-      hasRowModify:false,
+      hasRowView: false,
+      hasRowModify: false,
       // height: 'calc(100vh - 160px)',
       size: 'small',
       actionBarWidth: 250,
@@ -24,5 +25,33 @@ window[648640265023] = {
       },
       zoomStatus: 'max'
     }
+  }
+};
+// 当日转诊名单
+window[649780482266] = {
+  title: '当日转诊名单',
+  name: 'TableData',
+  props: {
+    resid: 649780568488,
+    isUseBESize: true,
+    subtractH: 200,
+    actionBarFixed: true,
+    hasDownload: false,
+    hasRowDelete: false,
+    hasZoomInOut: true,
+    height: 500,
+    afterSaveRefresh: true,
+    hasAdd: false,
+    hasRowView: false,
+    hasRowModify: false,
+    // height: 'calc(100vh - 160px)',
+    size: 'small',
+    actionBarWidth: 250,
+    hasModify: false,
+    hasDelete: false,
+    formProps: {
+      // displayMode: "classify",
+    },
+    zoomStatus: 'max'
   }
 };
