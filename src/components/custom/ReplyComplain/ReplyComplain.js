c5e40484a74570c2384c76f13fb7295f2c0dda99
GITDengMing <13767267543@163.com>
2020-10-23 10:05:12 +0800

    员工投诉图片证据的缩放条修改
    员工投诉图片证据的缩放条修改

--- src/components/custom/ReplyComplain/ReplyComplain.js
+++ src/components/custom/ReplyComplain/ReplyComplain.js
@@ -827,8 +827,7 @@ class ReplyComplain extends React.Component {
           className="changeAntCSS"
           visible={this.state.enlargePic}
           width={'90vw'}
-          style={{
-            
+          style={{ 
             height: '90vh',
             marginBottom: 0,
             paddingBottom: 0,
@@ -839,8 +838,7 @@ class ReplyComplain extends React.Component {
           onCancel={() => this.setState({ enlargePic: false })}
           destroyOnClose={true}
           footer={null}
-        >
-          
+        >  
           <img
             ref = {this.pic}
             id="pic"
@@ -848,18 +846,18 @@ class ReplyComplain extends React.Component {
             style={{
               transformOrigin:'top left',
               transform: `scale(${this.state.imgDeatilSize})`,
-              // transformOrigin: 'left',
               height: 'calc(100vh - 48px)',
               width: 'auto',
             }}
           />         
           <Slider
             style={{
-              left: '180px',
               position: 'fixed',
+              left:'0',
+              right:'0',
+              margin:'auto',
               bottom: '5vh',
               width: '1000px',
-              opacity:'0.3'
             }}
             defaultValue={1}
             step={0.1}
