1b62b99edee795c0eaa02f74110730ac9a263346
GITDengMing <13767267543@163.com>
2021-01-27 18:31:19 +0800

    修复年假管理和个人查看
    1.更改年假管理目录结构
    2.调整Tab名称
    3.调整Tab页提示信息
    4.年假个人查看的信息拆分为两个Tab
    5.增加年初创建API调用

--- src/components/custom/AnnualLeaveQuery/AnnualLeaveQuery.js
+++ src/components/custom/AnnualLeaveQuery/AnnualLeaveQuery.js
@@ -56,6 +56,14 @@ class AnnualLeaveQuery extends React.Component {
               userCode={UserCode}
             />
           </TabPane> */}
+          <TabPane tab="年假月度结算" key="3">
+            <CopySummary
+              baseURL={baseURL}
+              subResid={员工年假使用明细表}
+              resid={员工年假季度账户表}
+              userCode={UserCode}
+            />
+          </TabPane>
         </Tabs>
       </div>
     );
@@ -140,66 +148,66 @@ class Summary extends React.PureComponent {
       key: 'ljsq'
       // width: 120,
     },
-    {
-      title: '当季分配年假天数',
-      children: [
-        {
-          title: '已用',
-          dataIndex: 'sydjfp',
-          key: 'sydjfp'
-        },
-        {
-          title: '未用',
-          dataIndex: 'djfp',
-          key: 'djfp'
-        },
-        {
-          title: '合计',
-          dataIndex: 'djhj',
-          key: 'djhj'
-        }
-      ]
-    },
-    {
-      title: '往季分配年假天数',
-      children: [
-        {
-          title: '已用',
-          dataIndex: 'ljsysjsy',
-          key: 'ljsysjsy'
-        },
-        {
-          title: '未用',
-          dataIndex: 'sjsy',
-          key: 'sjsy'
-        },
-        {
-          title: '合计',
-          dataIndex: 'wjhj',
-          key: 'wjhj'
-        }
-      ]
-    },
-    {
-      title: '往年分配年假天数',
-      children: [
-        {
-          title: '已用',
-          dataIndex: 'ljsysnsy',
-          key: 'ljsysnsy'
-        },
-        {
-          title: '未用',
-          dataIndex: 'snsy',
-          key: 'snsy'
-        },
-        {
-          title: '合计',
-          dataIndex: 'wnhj',
-          key: 'wnhj'
-        }
-      ]
-    },
+    // {
+    //   title: '当季分配年假天数',
+    //   children: [
+    //     {
+    //       title: '已用',
+    //       dataIndex: 'sydjfp',
+    //       key: 'sydjfp'
+    //     },
+    //     {
+    //       title: '未用',
+    //       dataIndex: 'djfp',
+    //       key: 'djfp'
+    //     },
+    //     {
+    //       title: '合计',
+    //       dataIndex: 'djhj',
+    //       key: 'djhj'
+    //     }
+    //   ]
+    // },
+    // {
+    //   title: '往季分配年假天数',
+    //   children: [
+    //     {
+    //       title: '已用',
+    //       dataIndex: 'ljsysjsy',
+    //       key: 'ljsysjsy'
+    //     },
+    //     {
+    //       title: '未用',
+    //       dataIndex: 'sjsy',
+    //       key: 'sjsy'
+    //     },
+    //     {
+    //       title: '合计',
+    //       dataIndex: 'wjhj',
+    //       key: 'wjhj'
+    //     }
+    //   ]
+    // },
+    // {
+    //   title: '往年分配年假天数',
+    //   children: [
+    //     {
+    //       title: '已用',
+    //       dataIndex: 'ljsysnsy',
+    //       key: 'ljsysnsy'
+    //     },
+    //     {
+    //       title: '未用',
+    //       dataIndex: 'snsy',
+    //       key: 'snsy'
+    //     },
+    //     {
+    //       title: '合计',
+    //       dataIndex: 'wnhj',
+    //       key: 'wnhj'
+    //     }
+    //   ]
+    // },
     {
       title: '是否结算',
       dataIndex: 'SendBack',
@@ -573,6 +581,490 @@ class Summary extends React.PureComponent {
               </Modal>
             </Panel>
           </Collapse>
+        </div>
+      </div>
+    );
+  }
+}
+
+class CopySummary extends React.PureComponent {
+  state = {
+    allAnnualLeaveQuery: [], //全部年假数据
+    years: [], //员工从入职到当前的年份
+    startYear: undefined,
+    startQuarter: 1,
+    endYear: undefined,
+    endQuarter: 4,
+    subTableModalVisible: false,
+    usesubTableModalVisible: false,
+    applyRecordsModalVisible: false,
+    selectedRecord: {},
+    selectedSubRecord: {}
+  };
+  columns = [
+    {
+      title: '年份',
+      dataIndex: 'year',
+      key: 'year'
+      // width: 100,
+    },
+    {
+      title: '季度',
+      dataIndex: 'quarter',
+      key: 'quarter'
+      // width: 100,
+    },
+    // {
+    //   title: '合计可用年假',
+    //   dataIndex: 'hjky',
+    //   key: 'hjky'
+    //   // width: 120,
+    // },
+    // {
+    //   title: '剩余可用年假',
+    //   dataIndex: 'synj',
+    //   key: 'synj'
+    //   // width: 120,
+    // },
+    // {
+    //   title: '累积申请',
+    //   dataIndex: 'ljsq',
+    //   key: 'ljsq'
+    //   // width: 120,
+    // },
+    {
+      title: '当季分配年假天数',
+      children: [
+        {
+          title: '已用',
+          dataIndex: 'sydjfp',
+          key: 'sydjfp'
+        },
+        {
+          title: '未用',
+          dataIndex: 'djfp',
+          key: 'djfp'
+        },
+        {
+          title: '合计',
+          dataIndex: 'djhj',
+          key: 'djhj'
+        }
+      ]
+    },
+    {
+      title: '往季分配年假天数',
+      children: [
+        {
+          title: '已用',
+          dataIndex: 'ljsysjsy',
+          key: 'ljsysjsy'
+        },
+        {
+          title: '未用',
+          dataIndex: 'sjsy',
+          key: 'sjsy'
+        },
+        {
+          title: '合计',
+          dataIndex: 'wjhj',
+          key: 'wjhj'
+        }
+      ]
+    },
+    {
+      title: '往年分配年假天数',
+      children: [
+        {
+          title: '已用',
+          dataIndex: 'ljsysnsy',
+          key: 'ljsysnsy'
+        },
+        {
+          title: '未用',
+          dataIndex: 'snsy',
+          key: 'snsy'
+        },
+        {
+          title: '合计',
+          dataIndex: 'wnhj',
+          key: 'wnhj'
+        }
+      ]
+    }
+    // {
+    //   title: '是否结算',
+    //   dataIndex: 'SendBack',
+    //   key: 'SendBack'
+    //   // width: 100,
+    // },
+    // {
+    //   title: '操作',
+    //   key: 'action',
+    //   render: (text, record) => (
+    //     <>
+    //       <span
+    //         className="table-action--view"
+    //         onClick={() => {
+    //           this.setState({
+    //             selectedRecord: record,
+    //             usesubTableModalVisible: true
+    //           });
+    //         }}
+    //       >
+    //         使用明细
+    //       </span>
+    //       {/* <span
+    //         className="table-action--view"
+    //         onClick={() => {
+    //           this.setState({
+    //             selectedRecord: record,
+    //             subTableModalVisible: true
+    //           });
+    //         }}
+    //       >
+    //         交易明细
+    //       </span> */}
+    //     </>
+    //   )
+    // }
+  ];
+  componentDidMount() {
+    this.fetchAnnualLeaves();
+  }
+  fetchAnnualLeaves = async () => {
+    const { resid, userCode, baseURL } = this.props;
+    const numID = JSON.parse(localStorage.getItem('userInfo')).UserInfo.EMP_ID;
+    const curYear = parseInt(moment().year());
+    const curQuarter = parseInt(moment().quarter());
+
+    let res1;
+    try {
+      res1 = await http({ baseURL }).getTable({
+        resid: '662169383744',
+        cmswhere: `NumberID = '${numID}' and Quarter = '${curYear}'`
+      });
+      this.setState({
+        snsy: res1.data.length !== 0 ? res1.data[0].Residue : 0
+      });
+    } catch (error) {
+      console.log(error.message);
+      message.info(error.message);
+    }
+    let res;
+    try {
+      res = await http({ baseURL }).getTable({
+        resid: '662169346288',
+        cmswhere: `numberID = ${numID} and year = ${curYear}`
+      });
+      let synj1 = 0;
+      let hjky = 0;
+      res.data.map(item => {
+        if (item.synj >= 0) {
+          synj1 = item.synj + synj1;
+        }
+        if (item.quarter === curQuarter) {
+          hjky = item.hjky;
+          console.table({ hjky });
+        }
+      });
+      this.setState({
+        synj: synj1,
+        djfp: hjky
+      });
+    } catch (err) {
+      message.error(err.message);
+      console.log(err);
+    }
+    try {
+      const res = await http({ baseURL }).getTable({
+        resid,
+        cmswhere: `numberID = '${userCode}'`
+      });
+      const yearSet = new Set();
+      res.data.forEach(item => {
+        item.sydjfp = item.sydjfp || 0;
+        item.djhj = item.sydjfp + item.djfp; //当季合计
+
+        item.ljsysjsy = item.ljsysjsy || 0;
+        item.wjhj = item.ljsysjsy + item.sjsy; //往季合计
+
+        item.ljsysnsy = item.ljsysnsy || 0;
+        item.wnhj = item.ljsysnsy + item.snsy; //往年合计
+        yearSet.add(item.year);
+      });
+      const years = [...yearSet];
+      this.setState({
+        allAnnualLeaveQuery: res.data,
+        years,
+        startYear: years[0],
+        endYear: years[0]
+      });
+    } catch (error) {
+      console.error(error);
+      message.error(error.message);
+    }
+  };
+
+  calcAnnualLeaves = memoize(
+    (
+      startYear,
+      startQuarter,
+      endYear,
+      endQuarter,
+      allAnnualLeaveQuery = []
+    ) => {
+      return allAnnualLeaveQuery.filter(item => {
+        return (
+          item.year >= startYear &&
+          item.quarter >= startQuarter &&
+          item.year <= endYear &&
+          item.quarter <= endQuarter
+        );
+      });
+    }
+  );
+
+  render() {
+    const {
+      selectedSubRecord,
+      applyRecordsModalVisible,
+      usesubTableModalVisible,
+      selectedRecord,
+      allAnnualLeaveQuery,
+      years,
+      startQuarter,
+      startYear,
+      endQuarter,
+      endYear,
+      subTableModalVisible,
+      snsy,
+      synj,
+      djfp
+    } = this.state;
+    const { subResid, resid, baseURL } = this.props;
+    const annualLeaves = this.calcAnnualLeaves(
+      startYear,
+      startQuarter,
+      endYear,
+      endQuarter,
+      allAnnualLeaveQuery
+    );
+    return (
+      <div className="alq-summary">
+        {/* <div>
+          <span>当季可用年假</span>
+          <span>{djfp}天</span>
+          <span>总结余</span>
+          <span>{synj}天</span>
+          <span>上年转结年假</span>
+          <span>{snsy}天</span>
+        </div> */}
+        <div className="collapseStyle">
+          {/* <Collapse
+            bordered={false}
+            style={{ background: '#f7f7f7', border: 0, overflow: 'hidden' }}
+          >
+            <Panel header="更多信息" key="1"> */}
+          <header className="alq-summary__header">
+            <div>
+              从
+              <Select
+                onChange={value => {
+                  this.setState({
+                    startYear: value
+                  });
+                }}
+                value={startYear}
+                size="small"
+                style={styles.selectStyle}
+              >
+                {years.map(item => {
+                  return <Option value={item}>{item}</Option>;
+                })}
+              </Select>
+              <Select
+                onChange={value => {
+                  this.setState({
+                    startQuarter: value
+                  });
+                }}
+                value={startQuarter}
+                size="small"
+                style={styles.selectStyle}
+              >
+                {quarters.map(item => {
+                  return <Option value={item.value}>{item.title}</Option>;
+                })}
+              </Select>
+              至
+              <Select
+                onChange={value => {
+                  this.setState({
+                    endYear: value
+                  });
+                }}
+                value={endYear}
+                size="small"
+                style={styles.selectStyle}
+              >
+                {years.map(item => {
+                  return <Option value={item}>{item}</Option>;
+                })}
+              </Select>
+              <Select
+                onChange={value => {
+                  this.setState({
+                    endQuarter: value
+                  });
+                }}
+                value={endQuarter}
+                size="small"
+                style={styles.selectStyle}
+              >
+                {quarters.map(item => {
+                  return <Option value={item.value}>{item.title}</Option>;
+                })}
+              </Select>
+              <Button
+                icon="download"
+                size="small"
+                onClick={() => {
+                  tableToExcel(
+                    document.querySelector('.annual-leave .ant-table-body')
+                      .innerHTML
+                  );
+                }}
+              >
+                下载
+              </Button>
+            </div>
+            <Icon
+              onClick={() => {
+                Modal.info({
+                  title: '提示',
+                  content:
+                    '1.在每个季度（或者年）的第一天，上个季度（或者年）剩余的年假将会被结算至当前季度。'
+                });
+              }}
+              type="question-circle"
+              style={{ color: '#1890ff' }}
+            />
+          </header>
+          <div>
+            <Table
+              className="annual-leave"
+              columns={this.columns}
+              dataSource={annualLeaves}
+              bordered
+              size="small"
+              pagination={false}
+              // scroll={{ x: 'calc(700px + 50%)', y: 240 }}
+            />
+          </div>
+          <Modal
+            title="年假月度使用情况"
+            visible={usesubTableModalVisible}
+            footer={null}
+            width="80vw"
+            onCancel={() => {
+              this.setState({ usesubTableModalVisible: false });
+            }}
+          >
+            <TableData
+              key={selectedRecord.REC_ID}
+              dataMode="sub"
+              resid={resid}
+              subresid={'662737017622'}
+              hostrecid={selectedRecord.REC_ID}
+              baseURL={baseURL}
+              subtractH={200}
+              hasAdd={false}
+              hasModify={false}
+              hasDelete={false}
+              hasRowEdit={false}
+              hasRowModify={false}
+              hasRowView={true}
+              hasRowDelete={false}
+              actionBarWidth={100}
+              height={500}
+              customRowBtns={[
+                (record, btnSize) => {
+                  return (
+                    <Button
+                      onClick={() => {
+                        this.setState({
+                          selectedSubRecord: record,
+                          applyRecordsModalVisible: true
+                        });
+                      }}
+                      size={btnSize}
+                    >
+                      申请记录
+                    </Button>
+                  );
+                }
+              ]}
+            />
+          </Modal>
+          <Modal
+            title="申请记录"
+            visible={applyRecordsModalVisible}
+            footer={null}
+            width="70vw"
+            onCancel={() => {
+              this.setState({ applyRecordsModalVisible: false });
+            }}
+          >
+            <TableData
+              key={selectedSubRecord.REC_ID}
+              dataMode="sub"
+              resid={resid}
+              subresid={'662737017622'}
+              hostrecid={selectedSubRecord.REC_ID}
+              baseURL={baseURL}
+              subtractH={200}
+              hasAdd={false}
+              hasModify={false}
+              hasDelete={false}
+              hasRowEdit={false}
+              hasRowModify={false}
+              hasRowView={true}
+              hasRowDelete={false}
+              actionBarWidth={100}
+              height={500}
+            />
+          </Modal>
+          <Modal
+            title="年假交易明细"
+            visible={subTableModalVisible}
+            footer={null}
+            width="80vw"
+            onCancel={() => {
+              this.setState({ subTableModalVisible: false });
+            }}
+          >
+            <TableData
+              key={selectedRecord.REC_ID}
+              dataMode="sub"
+              resid={resid}
+              subresid={subResid}
+              hostrecid={selectedRecord.REC_ID}
+              baseURL={baseURL}
+              subtractH={200}
+              hasAdd={false}
+              hasModify={false}
+              hasDelete={false}
+              hasRowEdit={false}
+              hasRowModify={false}
+              hasRowView={true}
+              hasRowDelete={false}
+              actionBarWidth={100}
+              height={500}
+            />
+          </Modal>
+          {/* </Panel>
+          </Collapse> */}
         </div>
       </div>
     );
