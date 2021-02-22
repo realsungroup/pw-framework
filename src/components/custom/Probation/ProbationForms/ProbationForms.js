580b926145b92b8d6820e0ecf011a83283cfbc02
GITDengMing <13767267543@163.com>
2021-02-22 15:28:24 +0800

    辅导记录被确认邮件重复发送
    辅导记录被确认邮件重复发送

--- src/components/custom/Probation/ProbationForms/ProbationForms.js
+++ src/components/custom/Probation/ProbationForms/ProbationForms.js
@@ -463,9 +463,10 @@ class ProbationForms extends React.Component {
           }
         });
       });
-      console.log(onTheJobTraining);
+      // console.log(onTheJobTraining);
       mentorshipRecord.forEach(item => {
         delete item.instructEmailIsSend;
+        delete item.C3_625594433726;
         subdata.push({
           resid: resid6,
           maindata: {
@@ -484,31 +485,31 @@ class ProbationForms extends React.Component {
               ...employeeInformation,
               instructor:
                 this.state.employeeInformation.instructor &&
-                  this.state.employeeInformation.instructor.indexOf('-') > -1
+                this.state.employeeInformation.instructor.indexOf('-') > -1
                   ? this.state.employeeInformation.instructor.split('-')[0]
                   : this.state.employeeInformation.instructor,
               instructorDirectorName:
                 this.state.employeeInformation.instructorDirectorName &&
-                  this.state.employeeInformation.instructorDirectorName.indexOf(
-                    '-'
-                  ) > -1
+                this.state.employeeInformation.instructorDirectorName.indexOf(
+                  '-'
+                ) > -1
                   ? this.state.employeeInformation.instructorDirectorName.split(
-                    '-'
-                  )[0]
+                      '-'
+                    )[0]
                   : this.state.employeeInformation.instructorDirectorName,
               C3_637084539039:
                 this.state.employeeInformation.C3_637084539039 &&
-                  this.state.employeeInformation.C3_637084539039.indexOf('-') > -1
+                this.state.employeeInformation.C3_637084539039.indexOf('-') > -1
                   ? this.state.employeeInformation.C3_637084539039.split('-')[0]
                   : this.state.employeeInformation.C3_637084539039,
               instructorDirectorName2:
                 this.state.employeeInformation.instructorDirectorName2 &&
-                  this.state.employeeInformation.instructorDirectorName2.indexOf(
-                    '-'
-                  ) > -1
+                this.state.employeeInformation.instructorDirectorName2.indexOf(
+                  '-'
+                ) > -1
                   ? this.state.employeeInformation.instructorDirectorName2.split(
-                    '-'
-                  )[0]
+                      '-'
+                    )[0]
                   : this.state.employeeInformation.instructorDirectorName2,
               _state: 'modified',
               _id: 1
@@ -646,7 +647,7 @@ class ProbationForms extends React.Component {
           return Modal.info({
             title: '提示',
             content: '没有试用期信息。',
-            onOk: () => { }
+            onOk: () => {}
           });
         }
         let probationObjectives = data[viewableTable.objectiveResid].filter(
@@ -805,7 +806,6 @@ class ProbationForms extends React.Component {
         label: `${user.C3_227192484125}`,
         key: user.C3_305737857578,
         memberId: user.C3_227192472953
-
       }));
 
       this.setState({
@@ -922,7 +922,7 @@ class ProbationForms extends React.Component {
   confirmMentor = async index => {
     try {
       const mentorshipRecord = [...this.state.mentorshipRecord];
-      console.log(this.state.employeeInformation.instructorID);
+      // console.log(this.state.employeeInformation.instructorID);
       await http().modifyRecords({
         resid: resid6,
         data: [
@@ -1299,7 +1299,7 @@ class ProbationForms extends React.Component {
           console.log(error);
         }
       },
-      onCancel() { }
+      onCancel() {}
     });
   };
 
@@ -2182,7 +2182,7 @@ class ProbationForms extends React.Component {
                   width: {
                     size:
                       employeeInformation.smmary &&
-                        employeeInformation.smmary.length > 61
+                      employeeInformation.smmary.length > 61
                         ? 4535
                         : 9070,
                     type: WidthType.DXA
@@ -2224,7 +2224,7 @@ class ProbationForms extends React.Component {
                   width: {
                     size:
                       employeeInformation.directorEvaluate &&
-                        employeeInformation.directorEvaluate.length > 61
+                      employeeInformation.directorEvaluate.length > 61
                         ? 4535
                         : 9070,
                     type: WidthType.DXA
@@ -2267,7 +2267,7 @@ class ProbationForms extends React.Component {
                   width: {
                     size:
                       employeeInformation.ManagerEvaluate &&
-                        employeeInformation.ManagerEvaluate.length > 61
+                      employeeInformation.ManagerEvaluate.length > 61
                         ? 4535
                         : 9070,
                     type: WidthType.DXA
@@ -2309,7 +2309,7 @@ class ProbationForms extends React.Component {
                   width: {
                     size:
                       employeeInformation.hrManagerEvaluate &&
-                        employeeInformation.hrManagerEvaluate.length > 61
+                      employeeInformation.hrManagerEvaluate.length > 61
                         ? 4535
                         : 9070,
                     type: WidthType.DXA
@@ -2470,22 +2470,22 @@ class ProbationForms extends React.Component {
                     保存
                   </Button>
                   {roleName === 'HR' &&
-                    this.state.flagHitBack == true &&
-                    this.state.flagAlreadyHit == 0 ? (
-                      <span>
-                        <Button
-                          style={{ marginRight: '8px' }}
-                          onClick={this.agreeApply}
-                        >
-                          同意自定义辅导员
+                  this.state.flagHitBack == true &&
+                  this.state.flagAlreadyHit == 0 ? (
+                    <span>
+                      <Button
+                        style={{ marginRight: '8px' }}
+                        onClick={this.agreeApply}
+                      >
+                        同意自定义辅导员
                       </Button>
-                        <Button onClick={this.disagreeApply} type="danger">
-                          驳回自定义辅导员
+                      <Button onClick={this.disagreeApply} type="danger">
+                        驳回自定义辅导员
                       </Button>
-                      </span>
-                    ) : (
-                      ''
-                    )}
+                    </span>
+                  ) : (
+                    ''
+                  )}
                   {
                     <span style={{ color: 'red' }}>
                       {this.state.flagAlreadyHit == 2
@@ -2589,7 +2589,7 @@ class ProbationForms extends React.Component {
                 style={{ width: 300 }}
                 placeholder="请选择课程"
                 optionFilterProp="children"
-                onSearch={val => { }}
+                onSearch={val => {}}
                 filterOption={(input, option) =>
                   option.props.children
                     .toLowerCase()
@@ -2644,7 +2644,7 @@ class ProbationForms extends React.Component {
                 style={{ width: 300 }}
                 placeholder="请选择课程"
                 optionFilterProp="children"
-                onSearch={val => { }}
+                onSearch={val => {}}
                 value={this.state.modifyInternalCourseData.course}
                 filterOption={(input, option) =>
                   option.props.children
