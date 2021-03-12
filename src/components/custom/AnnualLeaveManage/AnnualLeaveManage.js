66839d1ddb17c50d4bd88ae6784483199f584561
codetanqi <50864263+skilusneka@users.noreply.github.com>
2021-03-12 16:55:29 +0800

    年假文字

--- src/components/custom/AnnualLeaveManage/AnnualLeaveManage.js
+++ src/components/custom/AnnualLeaveManage/AnnualLeaveManage.js
@@ -405,8 +405,8 @@ class AnnualLeaveManage extends React.Component {
       }
     },
     {
-      title: '年假概览',
-      tip: '年假概览',
+      title: '年假台账管理',
+      tip: '年假台账管理',
       render: () => {
         const { baseURL, baseURLFromAppConfig } = this.props;
         return (
@@ -577,9 +577,9 @@ class AnnualLeaveManage extends React.Component {
               });
             }}
           >
-            <Menu.Item key='年假概览'>
+            <Menu.Item key='年假台账管理'>
               <div className="menu-item__body">
-                年假概览
+                年假台账管理
                     </div>
             </Menu.Item>
             <SubMenu key="submenu1" title="交易明细">
@@ -649,7 +649,7 @@ class AnnualLeaveManage extends React.Component {
               if (index >= 9) {
                 return (
 
-                  menu.title === '年假概览' ? null :
+                  menu.title === '年假台账管理' ? null :
 
                     <Menu.Item key={menu.title}>
                       <div className="menu-item__body">
@@ -1968,16 +1968,17 @@ class NianJiaGaiLan extends React.PureComponent {
                   refTargetComponentName="TableData"
                   resid={663248318082}
                   baseURL={baseURL}
-                  subtractH={190}
+                  subtractH={220}
+                  hasAdvSearch={true}
                   hasAdd={true}
-                  hasModify={false}
-                  hasDelete={false}
-                  hasRowEdit={false}
-                  hasRowModify={false}
-                  hasRowView={true}
+                  hasRowView={false}
                   hasRowDelete={false}
-                  actionBarWidth={100}
-                  defaultAddRecord={{ C3_663248494033: selectedData.number }}
+                  hasRowEdit={false}
+                  hasDelete={false}
+                  hasModify={false}
+                  hasBeBtns={false}
+                  hasRowModify={false}
+                  hasRowSelection={false}
                   // actionBarExtra={this.actionBarExtra}
                   cmswhere={selectedData.number ? `C3_663248494033 = ${selectedData.number}` : '1 != 1'}
                   downloadBaseURL={baseURLFromAppConfig}
@@ -1996,12 +1997,12 @@ class NianJiaGaiLan extends React.PureComponent {
                   subtractH={190}
                   hasAdd={true}
                   hasModify={false}
-                  hasDelete={false}
-                  hasRowEdit={false}
+                  hasDelete={true}
+                  hasRowEdit={true}
                   hasRowModify={true}
                   hasRowView={true}
                   hasRowDelete={false}
-                  actionBarWidth={100}
+                  actionBarWidth={180}
                   downloadBaseURL={baseURLFromAppConfig}
                   defaultAddRecord={{ numberID: selectedData.number }}
                   cmswhere={selectedData.number ? `numberID = ${selectedData.number}` : '1 != 1'}
