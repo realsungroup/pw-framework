b19af506a5ac3ba28be7577033f0a9171654a07c
codetanqi <50864263+skilusneka@users.noreply.github.com>
2021-04-07 18:51:36 +0800

    fix:修复报错

--- src/components/common/ui/Control/Control.js
+++ src/components/common/ui/Control/Control.js
@@ -260,6 +260,7 @@ class Control extends React.Component {
       let filedsValue = {};
       clearOnChangeFileds.reduce((preV, currentV) => {
         preV[currentV] = undefined;
+        return preV;
       }, filedsValue);
       this.props.form.setFieldsValue(filedsValue);
     }
