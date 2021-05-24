632698d4461a0198bdc71613c654b7904c5db62a
MuxinFeng <434980373@qq.com>
2021-05-24 09:48:09 +0800

    人事调动下载按钮

--- src/components/custom/IDLTransferVerifyAction/IDLTransferVerifyAction.js
+++ src/components/custom/IDLTransferVerifyAction/IDLTransferVerifyAction.js
@@ -919,6 +919,7 @@ class IDLTransferVerifyAction extends Component {
                 hasRowModify={false}
                 hasModify={false}
                 hasDelete={false}
+                hasDownload={false}
                 hasRowView={false}
               />
             </div>
@@ -985,6 +986,7 @@ class IDLTransferVerifyAction extends Component {
               hasRowView={false}
               actionBarWidth={100}
               actionBarFixed={true}
+              hasDownload={false}
               subtractH={175}
               // noWidthFields={this.state.selection == '1' ? 'C3_634660565034' : null}
               actionBarExtra={({ dataSource, selectedRowKeys }) => {
