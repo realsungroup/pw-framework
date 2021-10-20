d62cf0e17c78a6b83cd77d6f10064ac258369bd0
MuxinFeng <>
2021-10-20 09:25:30 +0800

    Update functions.config.js

--- public/functions.config.js
+++ public/functions.config.js
@@ -10408,3 +10408,84 @@ window[687629239980] = {
     choosePeopleResid: 678368669087
   }
 };
+window[687976347225] = {
+  name: 'TabsTableData',
+  title: '上海访客审批',
+  props: [
+    [
+      {
+        resid: 687801974358,
+        TabsTitle: '未审批',
+        recordFormFormWidth: '90%',
+        hasBeBtns: true,
+        hasModify: false,
+        hasDelete: false,
+        hasAdd: false,
+        hasRowDelete: false,
+        hasRowModify: false,
+        hasRowView: true,
+        subtractH: 220
+      },
+      {
+        resid: 687823032125,
+        TabsTitle: '已审批',
+        recordFormFormWidth: '90%',
+        hasBeBtns: true,
+        hasModify: false,
+        hasDelete: false,
+        hasAdd: false,
+        hasRowDelete: false,
+        hasRowModify: false,
+        hasRowView: false,
+        subtractH: 220
+      }
+    ]
+  ]
+};
+window[687976386945] = {
+  name: 'TabsTableData',
+  title: '上海访客前台登记',
+  props: [
+    [
+      {
+        resid: 687801999108,
+        TabsTitle: '全部',
+        recordFormFormWidth: '90%',
+        hasBeBtns: true,
+        hasModify: false,
+        hasDelete: false,
+        hasAdd: false,
+        hasRowDelete: false,
+        hasRowModify: false,
+        hasRowView: true,
+        subtractH: 220
+      },
+      {
+        resid: 687823076582,
+        TabsTitle: '访问中',
+        recordFormFormWidth: '90%',
+        hasBeBtns: true,
+        hasModify: false,
+        hasDelete: false,
+        hasAdd: false,
+        hasRowDelete: false,
+        hasRowModify: false,
+        hasRowView: true,
+        subtractH: 220
+      },
+      {
+        resid: 687823090661,
+        TabsTitle: '访问结束',
+        recordFormFormWidth: '90%',
+        hasBeBtns: true,
+        hasModify: false,
+        hasDelete: false,
+        hasAdd: false,
+        hasRowDelete: false,
+        hasRowModify: false,
+        hasRowView: false,
+        subtractH: 220
+      }
+    ]
+  ]
+};
