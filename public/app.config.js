6209fff20a1e5941140c0d42f4fe58dcf7107458
MuxinFeng <>
2021-11-22 17:55:59 +0800

    附件下载

--- public/app.config.js
+++ public/app.config.js
@@ -8,7 +8,7 @@ window.pwConfig = {
     // baseURL: 'http://10.108.11.36:6001/', // 请求的基地址
     // baseURL: 'http://pwkwebapi.realsun.me/',
     // baseURL: 'http://10.108.2.66:1001/', // 请求的基地址
-    baseURL: 'http://10.108.2.66:6001/', // 请求的基地址
+    baseURL: 'http://10.108.2.66:1001/', // 请求的基地址
     basename: '/',
     // baseUrl: 'http://114.55.58.57:9096/', // 域登录基地址
     // baseURL: 'http://pwhrapi1.realsun.me/', // 域登录基地址
