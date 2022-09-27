f652f4a5825919145db83ff75ef86c1b8639e02b
codetanqi <50864263+skilusneka@users.noreply.github.com>
2022-09-27 18:13:00 +0800

    修改上海访客

--- src/components/custom/ShVisitManager/ShVisitManager.js
+++ src/components/custom/ShVisitManager/ShVisitManager.js
@@ -13,56 +13,91 @@ import './ShVisitManager.less';
 const { Option } = Select;
 
 /**
- * 上海访客审批
+ * 上海访客前台登记
  */
 export default class ShVisitManager extends Component {
   constructor(props) {
     super(props);
-    this.state={
-      fileName:'',
-      fileUrl:'',
-      activated:'',
-      showModal:false,
-      loading:false,
-      REC_ID:'',
-      isAdd:false
+    this.state = {
+      fileName: '',
+      fileUrl: '',
+      activated: '',
+      showModal: false,
+      loading: false,
+      REC_ID: '',
+      isAdd: false,
+      fileArr: []
     }
   }
   componentDidMount() {
   }
-  openModal=(v,a)=>{
-    if(v){
+  setFiles = (record) => {
+    let fileArr = [
+      {
+        fileUrl: record.fileUrl,
+        fileUrl2: record.fileUrl2,
+        fileUrl3: record.fileUrl3,
+      },
+      {
+        fileUrl: record.fileUrlA,
+        fileUrl2: record.fileUrl2A,
+        fileUrl3: record.fileUrl3A,
+      }, {
+        fileUrl: record.fileUrlB,
+        fileUrl2: record.fileUrl2B,
+        fileUrl3: record.fileUrl3B,
+      }, {
+        fileUrl: record.fileUrlC,
+        fileUrl2: record.fileUrl2C,
+        fileUrl3: record.fileUrl3C,
+      }, {
+        fileUrl: record.fileUrlD,
+        fileUrl2: record.fileUrl2D,
+        fileUrl3: record.fileUrl3D,
+      }, {
+        fileUrl: record.fileUrlE,
+        fileUrl2: record.fileUrl2E,
+        fileUrl3: record.fileUrl3E,
+      }
+    ]
+    this.setState({
+      fileArr,
+      modalVis: true
+    })
+  }
+  openModal = (v, a) => {
+    if (v) {
       this.setState({
-        fileName:v.fileName,
-        fileUrl:v.fileUrl,
-        activated:v.activated,
-        showModal:true,
-        REC_ID:v.REC_ID,
-        isAdd:false
+        fileName: v.fileName,
+        fileUrl: v.fileUrl,
+        activated: v.activated,
+        showModal: true,
+        REC_ID: v.REC_ID,
+        isAdd: false
       })
-    }else{
+    } else {
       this.setState({
-        fileName:'',
-        fileUrl:'',
-        activated:'',
-        showModal:true,
-        REC_ID:'',
-        isAdd:true
+        fileName: '',
+        fileUrl: '',
+        activated: '',
+        showModal: true,
+        REC_ID: '',
+        isAdd: true
       })
     }
-   
+
   }
-  handleUpload=(e)=>{
+  handleUpload = (e) => {
     let files = e.target.files || e.dataTransfer.files;
 
     if (!files.length) return;
     let type = files[0].name.split('.');
-    let size = files[0].size; 
+    let size = files[0].size;
     if (size > 5242880) {
       alert("请选择5M以内的文件！");
       return false;
     }
-    this.uploadFile(files[0], `http://kingofdinner.realsun.me:1201/api/AliyunOss/PutOneImageObject?bucketname=nutritiontower&srctype=${type[type.length-1]}`, "cloud").then((result) => {
+    this.uploadFile(files[0], `http://kingofdinner.realsun.me:1201/api/AliyunOss/PutOneImageObject?bucketname=nutritiontower&srctype=${type[type.length - 1]}`, "cloud").then((result) => {
 
       this.setState({ loading: false, fileUrl: result })
 
@@ -93,198 +128,197 @@ export default class ShVisitManager extends Component {
       xhr.send(fd);
     });
   };
-  handleSub=async()=>{
+  handleSub = async () => {
     let res;
-    try{
-      this.setState({loading:true});
+    try {
+      this.setState({ loading: true });
       res = await http().modifyRecords({
-        resid:700158571490,
-        data:[this.state]
+        resid: 700158571490,
+        data: [this.state]
       })
       this.clearState();
       message.success('成功');
       this.tableDataRef.handleRefresh();
-    }catch(e){
+    } catch (e) {
       console.log(e.message);
       message.error(e.message)
-      this.setState({loading:false})
+      this.setState({ loading: false })
     }
   }
-  handleAdd=async()=>{
+  handleAdd = async () => {
     let res;
-    try{
-      this.setState({loading:true});
+    try {
+      this.setState({ loading: true });
       res = await http().addRecords({
-        resid:700158571490,
-        data:[this.state]
+        resid: 700158571490,
+        data: [this.state]
       })
-     this.clearState();
+      this.clearState();
       message.success('成功');
       this.tableDataRef.handleRefresh();
-    }catch(e){
+    } catch (e) {
       console.log(e.message);
       message.error(e.message)
-      this.setState({loading:false})
+      this.setState({ loading: false })
     }
   }
-  clearState=()=>{
+  clearState = () => {
     this.setState({
-      fileName:'',
-      loading:false,
-      fileUrl:'',
-      activated:'',
-      showModal:false,
-      REC_ID:''
+      fileName: '',
+      loading: false,
+      fileUrl: '',
+      activated: '',
+      showModal: false,
+      REC_ID: ''
     })
   }
   render() {
     return (
       <div className="table-data-wrap">
-       <Tabs defaultActiveKey="1" size="small">
+        <Modal
+          visible={this.state.modalVis}
+          onCancel={() => { this.setState({ modalVis: false, fileArr: [] }) }}
+          footer={null}
+          destroyOnClose
+          title={'查看附件'}
+          width={800}
+        >
+          <table>
+            <tr>
+              <td>
+                序号
+                  </td>
+              <td>
+                来访人员表
+                  </td>
+              <td>
+                访客绿码
+                  </td>
+              <td>
+                行动轨迹
+                  </td>
+            </tr>
+            {
+              this.state.fileArr.map(
+                (item, key) => {
+                  return (
+                    <tr>
+                      <td>
+                        {key + 1}
+                      </td>
+                      <td>
+                        <a target='
+                            _blank' href={item.fileUrl}>{item.fileUrl}</a>
+                      </td>
+                      <td>
+                        <a target='
+                            _blank' href={item.fileUrl2}>{item.fileUrl2}</a>
+                      </td>
+                      <td>
+                        <a target='
+                            _blank' href={item.fileUrl3}>{item.fileUrl3}</a>
+                      </td>
+                    </tr>
+                  )
+                }
+              )
+            }
+          </table>
+        </Modal>
+        <Tabs defaultActiveKey="1" size="small">
           <Tabs.TabPane tab="全部" key="1">
-          <div className='wrap'>
-
-          <TableData
-          resid={'687801999108'}
-          hasRowView={false}
-          hasAdd={false}
-          hasRowDelete={false}
-          hasRowModify={false}
-          hasModify={false}
-          hasDelete={false}
-          hasBeBtns={true}
-          subtractH={175}
-          hasRowView={true}
-          customRowBtns={[
-            record => {
-              return (
-                <>
-                  <Button
-                    onClick={() => {
-                      window.open(record.fileUrl)
-                    }}
-                  >
-                    下载来访人员表
-                  </Button>
-                  <Button
-                    onClick={() => {
-                      window.open(record.fileUrl2)
-                    }}
-                  >
-                   下载访客绿码
-                  </Button>
-                  <Button
-                    onClick={() => {
-                      window.open(record.fileUrl3)
-                    }}
-                  >
-                    下载行动轨迹
-                  </Button>
-                </>
-              );
-            }
-          ]}
-        />
-        </div>
-            </Tabs.TabPane>
-          <Tabs.TabPane tab="访问中" key="2">
-          <div className='wrap'>
-
-          <TableData
-          resid={'687823076582'}
-          hasRowView={false}
-          hasAdd={false}
-          hasRowDelete={false}
-          hasRowModify={false}
-          hasModify={false}
-          hasDelete={false}
-          subtractH={175}
-          hasBeBtns={true}
-          hasRowView={true}
-          customRowBtns={[
-            record => {
-              return (
-                <>
-                  <Button
-                    onClick={() => {
-                      window.open(record.fileUrl)
-                    }}
-                  >
-                    下载来访人员表
-                  </Button>
-                  <Button
-                    onClick={() => {
-                      window.open(record.fileUrl2)
-                    }}
-                  >
-                   下载访客绿码
-                  </Button>
-                  <Button
-                    onClick={() => {
-                      window.open(record.fileUrl3)
-                    }}
-                  >
-                    下载行动轨迹
-                  </Button>
-                </>
-              );
-            }
-          ]}
-        />
-        </div>
-            </Tabs.TabPane>
-            <Tabs.TabPane tab="访问结束" key="3">
             <div className='wrap'>
 
-          <TableData
-          resid={'687823090661'}
-          hasRowView={false}
-          hasAdd={false}
-          hasRowDelete={false}
-          hasRowModify={false}
-          hasModify={false}
-          hasDelete={false}
-          subtractH={175}
-          hasRowView={true}
-          hasBeBtns={true}
-          customRowBtns={[
-            record => {
-              return (
-                <>
-                  <Button
-                    onClick={() => {
-                      window.open(record.fileUrl)
-                    }}
-                  >
-                    下载来访人员表
-                  </Button>
-                  <Button
-                    onClick={() => {
-                      window.open(record.fileUrl2)
-                    }}
-                  >
-                   下载访客绿码
-                  </Button>
-                  <Button
-                    onClick={() => {
-                      window.open(record.fileUrl3)
-                    }}
-                  >
-                    下载行动轨迹
-                  </Button>
-                </>
-              );
-            }
-          ]}
-        /></div>
-            </Tabs.TabPane>
-            <Tabs.TabPane tab="文件模板管理" key="4">
+              <TableData
+                resid={'687801999108'}
+                hasRowView={false}
+                hasAdd={false}
+                hasRowDelete={false}
+                hasRowModify={false}
+                hasModify={false}
+                hasDelete={false}
+                hasBeBtns={true}
+                subtractH={175}
+                hasRowView={true}
+                customRowBtns={[
+                  record => {
+                    return (<Button
+                      onClick={() => {
+                        this.setFiles(record)
+                      }}
+                    >
+                      查阅附件
+                    </Button>
+                    );
+                  }
+                ]}
+              />
+            </div>
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="访问中" key="2">
+            <div className='wrap'>
+
+              <TableData
+                resid={'687823076582'}
+                hasRowView={false}
+                hasAdd={false}
+                hasRowDelete={false}
+                hasRowModify={false}
+                hasModify={false}
+                hasDelete={false}
+                subtractH={175}
+                hasBeBtns={true}
+                hasRowView={true}
+                customRowBtns={[
+                  record => {
+                    return (<Button
+                      onClick={() => {
+                        this.setFiles(record)
+                      }}
+                    >
+                      查阅附件
+                    </Button>
+                    );
+                  }
+                ]}
+              />
+            </div>
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="访问结束" key="3">
+            <div className='wrap'>
+
+              <TableData
+                resid={'687823090661'}
+                hasRowView={false}
+                hasAdd={false}
+                hasRowDelete={false}
+                hasRowModify={false}
+                hasModify={false}
+                hasDelete={false}
+                subtractH={175}
+                hasRowView={true}
+                hasBeBtns={true}
+                customRowBtns={[
+                  record => {
+                    return (<Button
+                      onClick={() => {
+                        this.setFiles(record);
+                      }}
+                    >
+                      查阅附件
+                    </Button>
+                    );
+                  }
+                ]}
+              /></div>
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="文件模板管理" key="4">
             <Modal
               visible={this.state.showModal}
               width={'80vw'}
               title={'文件模板'}
               onCancel={() => {
-                this.setState({showModal:false});
+                this.setState({ showModal: false });
               }}
               destroyOnClose
               footer={
@@ -293,9 +327,9 @@ export default class ShVisitManager extends Component {
                     loading={this.state.loading}
                     type="primary"
                     onClick={() => {
-                      if(this.state.isAdd){
+                      if (this.state.isAdd) {
                         this.handleAdd();
-                      }else{
+                      } else {
                         this.handleSub();
                       }
                     }}
@@ -310,82 +344,83 @@ export default class ShVisitManager extends Component {
                   <span>文件名：</span>
                   <Input
                     value={this.state.fileName}
-                    onChange={(v)=>{this.setState({fileName:v.target.value})}}
+                    onChange={(v) => { this.setState({ fileName: v.target.value }) }}
                   />
                 </div>
                 <div>
                   <span>是否生效：</span>
                   <Select
-                  style={{ width: 120, marginRight: 16 }}
-                  size="small"
-                  onChange={v => {
-                    this.setState({ activated: v });
-                  }}
-                  value={this.state.activated}
+                    style={{ width: 120, marginRight: 16 }}
+                    size="small"
+                    onChange={v => {
+                      this.setState({ activated: v });
+                    }}
+                    value={this.state.activated}
                   >
                     <Select.Option value=""></Select.Option>
                     <Select.Option value="Y">Y</Select.Option>
                   </Select>
                 </div>
                 <div>
-                <b style={{color:'#f5222d'}}>*</b>上传文件：<input id="ss" name="ss" type="file" onChange={v => { this.handleUpload(v) }}/>
+                  <b style={{ color: '#f5222d' }}>*</b>上传文件：<input id="ss" name="ss" type="file" onChange={v => { this.handleUpload(v) }} />
                 </div>
               </div>
             </Modal>
             <div className='wrap'>
 
-          <TableData
-          resid={'700158437331'}
-          hasRowView={false}
-          hasAdd={false}
-          hasRowDelete={true}
-          hasRowModify={false}
-          hasModify={false}
-          hasDelete={true}
-          subtractH={175}
-          hasBeBtns={true}
-          wrappedComponentRef={element => (this.tableDataRef = element)}
-          refTargetComponentName="TableData"
-          hasRowView={true}
-          actionBarExtra={({ dataSource, selectedRowKeys }) => {
-            return (
-              <>
-                <Button 
-                size={'small'}
-                onClick={()=>{
-                  this.openModal(null,'Add');
-                }}>
-                  添加
+              <TableData
+                resid={'700158437331'}
+                hasRowView={false}
+                hasAdd={false}
+                hasRowDelete={true}
+                hasRowModify={false}
+                hasModify={false}
+                hasDelete={true}
+                subtractH={175}
+                hasBeBtns={true}
+                wrappedComponentRef={element => (this.tableDataRef = element)}
+                refTargetComponentName="TableData"
+                hasRowView={true}
+                actionBarExtra={({ dataSource, selectedRowKeys }) => {
+                  return (
+                    <>
+                      <Button
+                        size={'small'}
+                        onClick={() => {
+                          this.openModal(null, 'Add');
+                        }}>
+                        添加
                 </Button>
-              </>
-          )}}
-          customRowBtns={[
-            record => {
-              return (
-                <>
-                  <Button
-                    onClick={
-                      ()=>{
-                        this.openModal(record);
-                      }
-                    }
-                  >
-                    修改
+                    </>
+                  )
+                }}
+                customRowBtns={[
+                  record => {
+                    return (
+                      <>
+                        <Button
+                          onClick={
+                            () => {
+                              this.openModal(record);
+                            }
+                          }
+                        >
+                          修改
                   </Button>
-                  <Button
-                      onClick={() => {
-                        window.open(record.fileUrl)
-                      }}
-                    >
-                      下载
+                        <Button
+                          onClick={() => {
+                            window.open(record.fileUrl)
+                          }}
+                        >
+                          下载
                     </Button>
-                </>
-              );
-            }
-          ]}
-        /></div>
-            </Tabs.TabPane>
-            
+                      </>
+                    );
+                  }
+                ]}
+              /></div>
+          </Tabs.TabPane>
+
         </Tabs>
       </div>
     );
