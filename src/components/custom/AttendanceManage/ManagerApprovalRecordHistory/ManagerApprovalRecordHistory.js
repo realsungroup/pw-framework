284b2f9c790f403ab419498a4ac6c63735f4860c
codetanqi <50864263+skilusneka@users.noreply.github.com>
2022-10-22 13:52:34 +0800

    前端生成老系统申请的附件到后台

--- src/components/custom/AttendanceManage/ManagerApprovalRecordHistory/ManagerApprovalRecordHistory.js
+++ src/components/custom/AttendanceManage/ManagerApprovalRecordHistory/ManagerApprovalRecordHistory.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './ManagerApprovalRecordHistory.less';
 import TableData from '../../../common/data/TableData';
-import { Button } from 'antd';
+import { Button, Modal } from 'antd';
+import http from '../../../../util20/api'
 /*
  * 经理审批记录历史
  */
@@ -9,10 +10,29 @@ import { Button } from 'antd';
 class ManagerApprovalRecordHistory extends React.Component {
   constructor(props) {
     super(props);
+
     this.baseURL =
       window.pwConfig[process.env.NODE_ENV].customURLs.attendanceBaseURL;
+    this.mediaFieldBaseURL =
+      window.pwConfig[process.env.NODE_ENV].customURLs.mediaFieldBaseURL;
     this.attendanceDownloadURL =
       window.pwConfig[process.env.NODE_ENV].customURLs.attendanceDownloadURL;
+  }
+  state = {
+    showLink: false,
+    curLink: ''
+  }
+  getLink = async (record) => {
+    try {
+      let res2 = await http({ baseURL: this.baseURL }).getBinImage({
+        resid: 516107915085,
+        colname: 'C3_451391568463',
+        recid: record.C3_446915623989,
+      });
+      this.setState({ curLink: res2.data ? this.mediaFieldBaseURL + res2.data : '', showLink: true })
+    } catch (e) {
+      console.log(e.error)
+    }
   }
   render() {
     const {
@@ -21,6 +41,20 @@ class ManagerApprovalRecordHistory extends React.Component {
     } = this.props;
     return (
       <div className="attendance-manage_tabledata__wrapper">
+        <Modal
+          title={'查阅附件'}
+          visible={this.state.showLink}
+          footer={null}
+          destroyOnClose
+          onCancel={() => { this.setState({ showLink: false, curLink: '' }) }}
+        >
+          <div>
+            <span>文件链接：</span>
+            {
+              this.state.curLink ? <a href={this.state.curLink} target='_blank'>{this.state.curLink}</a> : '无'
+            }
+          </div>
+        </Modal>
         <TableData
           key="invalid-application-form"
           resid="470689569411"
@@ -48,7 +82,11 @@ class ManagerApprovalRecordHistory extends React.Component {
                   <Button
                     size={size}
                     onClick={() => {
-                      window.open(record.fileUrl)
+                      if (record.fileUrl && record.fileUrl != 'http://wux-hr03.chn.ii-vi.net') {
+                        this.setState({ curLink: record.fileUrl, showLink: true })
+                      } else {
+                        this.getLink(record)
+                      }
                     }}
                   >
                     {locale == 'en' ? 'Attachments' : '查看附件'}
