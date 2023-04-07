5b7c55ae51c7623ff292281bfe42c334189cd6e1
codetanqi <50864263+skilusneka@users.noreply.github.com>
2023-04-07 16:01:27 +0800

    Update functions.config.js

--- public/functions.config.js
+++ public/functions.config.js
@@ -10681,3 +10681,27 @@ window[732215086446] = {
   } // 组件所接收的 props
 };
 
+window[734182338434] = {
+  name: 'TableData', // 组件名称，这里为定制组件名称
+  title: '育儿假台账—考勤员用', // 功能模块名称
+  hasBackBtn: true, // 是否有返回上一页的按钮，默认为 true
+  props: {
+    resid: 734175287105,
+    baseURL: 'http://10.108.2.66:9091/',
+    downloadBaseURL: 'http://10.108.2.66:80/',
+    recordFormFormWidth: '90%',
+    hasAdd: false,
+    hasBeBtns: false,
+    hasModify: false,
+    hasDelete: true,
+    hasRowModify: false,
+    hasRowView: false,
+    hasRowDelete: false,
+    hasBeBtns: true,
+    hasAdvSearch: true,
+    height: '100vh',
+    recordFormFormWidth: '90%',
+    subtractH: 240
+  } // 组件所接收的 props
+};
+
